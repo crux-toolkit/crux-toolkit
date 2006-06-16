1ce8dc131c4c51a1958e01009cf421d0dc9ce27e
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2006-06-16 21:53:04 +0000

    *** empty log message ***

--- src/c/spectrum.c
+++ src/c/spectrum.c
@@ -3,7 +3,7 @@
  * AUTHOR: Tobias P. Mann
  * CREATE DATE: 19 Sept 2003
  * DESCRIPTION: code to support working with spectra
- * REVISION: $Revision: 1.1 $
+ * REVISION: $Revision: 1.2 $
  ****************************************************************************/
 #include <math.h>
 #include <stdio.h>
@@ -15,4 +15,22 @@
 #include "peak.h"
 #include "utils.h"
 
-  
+/**
+ * \struct spectrum 
+ */
+struct spectrum {
+  int               first_scan;   ///< The number of the first scan
+	int               last_scan;    ///< The number of the last scan
+  int               id;           ///< A unique identifier
+  SPECTRUM_TYPE_T   spectrum_type;///< The type of spectrum.
+  float             precursor_mz; ///< The m/z of the precursor (for MS-MS spectra)
+  int*              possible_z;   ///< The possible charge states of this spectrum
+  PEAK_T*           peaks;        ///< The spectrum peaks
+  float             min_peak_mz;  ///< The minimum m/z of all peaks
+  float             max_peak_mz;  ///< The maximum m/z of all peaks
+  int               num_peaks;    ///< The number of peaks
+  double            total_energy; ///< The sum of intensities in all peaks
+  char*             filename;     ///< Optional filename
+};    
+
+
