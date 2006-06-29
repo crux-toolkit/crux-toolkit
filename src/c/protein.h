af7751ba6013a907b4c8d399bd79a08a6cf61eac
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2006-06-29 22:22:55 +0000

    *** empty log message ***

--- src/c/protein.h
+++ src/c/protein.h
@@ -1,6 +1,6 @@
 /**
  * \file protein.h 
- * $Revision: 1.1 $
+ * $Revision: 1.2 $
  * \brief Object for representing one protein sequence.
  *****************************************************************************/
 #ifndef PROTEIN_H 
@@ -72,6 +72,36 @@ BOOLEAN_T parse_protein_fasta_file(
 
 /*int get_protein_peptides(PROTEIN_T* protein);*/
 /*int get_protein_peptides(PROTEIN_T* protein);*/
+
+
+/**
+ * Instantiates a new peptide_iterator from a peptide.
+ * \returns a PEPTIDE_ITERATOR_T object.
+ */
+
+/**
+ * \typedef PEPTIDE_ITERATOR_T
+ */
+typedef struct peptide_iterator PEPTIDE_ITERATOR_T;
+
+
+PEPTIDE_ITERATOR_T* new_peptide_iterator(PROTEIN_T* protein);        
+
+/**
+ * Frees an allocated peptide_iterator object.
+ */
+void free_peptide_iterator(PEPTIDE_ITERATOR_T* peptide_iterator);
+
+/**
+ * The basic iterator functions.
+ * \returns TRUE if there are additional peptides to iterate over, FALSE if not.
+ */
+BOOLEAN_T peptide_iterator_has_next(PEPTIDE_ITERATOR_T* peptide_iterator);
+
+/**
+ * \returns The next peptide (a character) in the peptide.
+ */
+PEPTIDE_T* peptide_iterator_next(PEPTIDE_ITERATOR_T* peptide_iterator);
 
 /*
  * Local Variables:
