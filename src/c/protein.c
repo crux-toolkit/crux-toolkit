f27ed69b55e2b034e6788a8a0061ba2e3feeaa28
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2006-07-04 00:14:02 +0000

    *** empty log message ***

--- src/c/protein.c
+++ src/c/protein.c
@@ -1,6 +1,6 @@
 /*****************************************************************************
  * \file protein.c
- * $Revision: 1.6 $
+ * $Revision: 1.7 $
  * \brief: Object for representing a single protein.
  ****************************************************************************/
 #include <stdio.h>
@@ -29,11 +29,11 @@ struct protein {
 };    
 
 /**
- * \struct peptide_iterator
+ * \struct protein_peptide_iterator
  * \brief Object to iterate over the peptides within a protein.
  */
-struct peptide_iterator {
-  PROTEIN_T* protein; ///< The spectrum whose peaks to iterate over. 
+struct protein_peptide_iterator {
+  PROTEIN_T* protein; ///< The protein whose peptides to iterate over. 
   unsigned short int cur_start; ///< Start in protein of the current peptide.
   unsigned char cur_length; ///< The length of the current peptide.
 };
