0139ed4f087dddde150fa6a29d7441791c45e67f
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2006-07-10 21:19:02 +0000

    *** empty log message ***

--- src/c/peptide.c
+++ src/c/peptide.c
@@ -1,6 +1,6 @@
 /*****************************************************************************
  * \file peptide.c
- * $Revision: 1.10 $
+ * $Revision: 1.11 $
  * \brief: Object for representing a single peptide.
  ****************************************************************************/
 #include <math.h>
@@ -29,7 +29,7 @@ struct peptide {
  * satisfy.
  */
 struct peptide_constraint {
-  PEPTIDE_TYPE_T*  peptide_type; ///< The type of peptides.
+  PEPTIDE_TYPE_T  peptide_type; ///< The type of peptides.
   float min_mass; ///< The minimum mass of the peptide
   float max_mass; ///< The maximum mass of the peptide
   int min_length; ///< The minimum length of the peptide
