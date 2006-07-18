64185b5bd430759d405bb543bf1508986c23388e
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2006-07-18 19:54:03 +0000

    *** empty log message ***

--- src/c/database.h
+++ src/c/database.h
@@ -1,6 +1,6 @@
 /**
  * \file database.h 
- * $Revision: 1.3 $
+ * $Revision: 1.4 $
  * \brief Object for representing a database of protein sequences.
  *****************************************************************************/
 #ifndef DATABASE_H
@@ -61,7 +61,7 @@ BOOLEAN_T parse_database(
 BOOLEAN_T get_database_protein_at_idx(
     DATABASE_T* database, ///< A parsed database object -in
     int protein_idx, ///< The index of the protein to retrieve -in
-    PROTEIN_T** ///< A pointer to a pointer to a PROTEIN object -out
+    PROTEIN_T** protein_ptr_ptr ///< A pointer to a pointer to a PROTEIN object -out
     );
 
 /** 
