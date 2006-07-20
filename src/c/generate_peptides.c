356b6a95fbff9fa1d1e4c9e52d2f523ef5ab1367
cpark <cpark@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2006-07-20 22:52:54 +0000

    added more files

---
+++ src/c/generate_peptides.c
@@ -0,0 +1,157 @@
+/*****************************************************************************
+ * \file generate_peptides
+ * AUTHOR: Chris Park
+ * CREATE DATE: July 17 2006
+ * DESCRIPTION: Given a protein fasta sequence database as input, generate a list of peptides in 
+ *              the database that meet certain criteria (e.g. mass, length, trypticity) as output.
+ * REVISION: 
+ ****************************************************************************/
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+#include <unistd.h>
+#include "peptide.h"
+#include "protein_peptide_association.h"
+#include "protein.h"
+#include "database.h"
+#include "parse_arguments.h"
+
+void wrong_command(char* arg){
+  fprintf(stderr, "ERROR: incorrect argument %s\n", arg);
+  fprintf(stderr, "%s", parse_arguments_get_usage("generate_peptides"));
+}
+
+int main(int argc, char** argv){
+
+  /* Set default values for any options here */
+  int flag_opt = FALSE;
+  double min_mass = 200;
+  double max_mass = 2400;
+  int min_length = 6;
+  int max_length = 50;
+  char* cleavages = "tryptic";   // or all
+  PEPTIDE_TYPE_T peptide_type = TRYPTIC;
+  int mis_cleavages = FALSE;
+  char* sort = "none";      // mass or length or none
+  char * in_file = NULL;
+  const char * error_message;
+  int result = 0;
+
+
+  /* Define optional command line arguments */
+  parse_arguments_set_opt("output-sequence", "Output the peptide sequence as well as the protein id and start and stop.", 
+                          (void *) &flag_opt, 
+                          FLAG_ARG);
+
+  parse_arguments_set_opt("min-mass", "The minimum neutral mass of the peptides to output", 
+                          (void *) &min_mass, 
+                          DOUBLE_ARG);
+
+  parse_arguments_set_opt("max-mass", "The maximum neutral mass of the peptides to output", 
+                          (void *) &max_mass, 
+                          DOUBLE_ARG);
+
+  parse_arguments_set_opt("min-length", "The minimum length of the peptides to output.",
+                          (void *) &min_length, 
+                          INT_ARG);
+
+  parse_arguments_set_opt("max-length", "The maximum length of the peptides to output.",
+                          (void *) &max_length, 
+                          INT_ARG);
+
+  parse_arguments_set_opt("cleavages", "Type of cleavages to allow. The tryptic option outputs peptides that result from a complete in silico digestion of proteins after residues K or R except when followed by P, wherease all outputs all possible peptides.", 
+                          (void *) &cleavages, 
+                          STRING_ARG);
+
+  parse_arguments_set_opt("mis-cleavages", "Allow mis cleavage sites with in a peptide.",
+                          (void *) &mis_cleavages, 
+                          FLAG_ARG);
+  
+  parse_arguments_set_opt("sort", "Specify the order in which peptides are printed to standard output. By default, the output is not sorted.", 
+                          (void *) &sort, 
+                          STRING_ARG);
+
+  /* Define required command line arguments */
+  parse_arguments_set_req("protein input filename", "The name of the file (in fasta format) from which to parse proteins.", 
+                          (void *) &in_file, STRING_ARG);
+
+
+  /* Parse the command line */
+  if (parse_arguments(argc, argv, 0)) {
+    PEPTIDE_CONSTRAINT_T* constraint;
+    DATABASE_PEPTIDE_ITERATOR_T* iterator;
+    DATABASE_T* database;
+    PEPTIDE_T* peptide;
+   
+    //FIXME may add additional types such as non-trypticc or partially-tryptic
+    if(strcmp(cleavages, "all")==0){
+      peptide_type = ANY_TRYPTIC;
+    }
+    else if(strcmp(cleavages, "tryptic")==0){
+      peptide_type = TRYPTIC;
+    }
+    else{
+      wrong_command(cleavages);
+      exit;
+    }
+
+    //peptide constraint
+    constraint = new_peptide_constraint(peptide_type, min_mass, max_mass, min_length, max_length, mis_cleavages);
+ 
+    //check if input file exist
+    if(access(in_file, F_OK)){
+      fprintf(stderr,"Fatal: The file \"%s\" does not exist (or is not readable, or is empty).\n", in_file);
+      exit(1);
+    }
+    
+    //print header line
+    printf("# PROTEIN DATABASE: %s\n", in_file);
+    printf("# OPTIONS:\n");
+    printf("\tmin-mass: %.2f\n", min_mass);
+    printf("\tmax-mass: %.2f\n", max_mass);
+    printf("\tmin-length: %d\n", min_length);
+    printf("\tmax-length: %d\n", max_length);
+    printf("\tcleavages: %s\n", cleavages);
+    if(mis_cleavages){
+      printf("\tallow mis-cleavages: TRUE\n");
+    }
+    else{
+      printf("\tallow mis-cleavages: FALSE\n");
+    }
+    printf("\tsort: %s (needs to be implemented)\n", sort);
+    
+
+    //create a new database
+    database = new_database(in_file);
+    //create peptide iterator
+    iterator = new_database_peptide_iterator(database, constraint);
+    
+    //check if any peptides are found
+    if(!database_peptide_iterator_has_next(iterator)){
+      printf("no matches found\n");
+    }
+    else{
+      //print each peptide
+      while(database_peptide_iterator_has_next(iterator)){
+        peptide = database_peptide_iterator_next(iterator);
+        print_peptide(peptide, stdout);
+        free_peptide(peptide);
+      }
+    }
+      //free database, iterator, constraint
+      free_database_peptide_iterator(iterator);
+      free_peptide_constraint(constraint);
+      free_database(database);
+  } 
+  else {
+    result = parse_arguments_get_error(&error_message);
+    fprintf(stderr, "Error in command line. Error # %d\n", result);
+    fprintf(stderr, "%s\n", error_message);
+    fprintf(stderr, "%s", parse_arguments_get_usage("generate_peptides"));
+    return result;
+  }
+  exit(1);
+}
---
+++ src/c/parse_arguments.h
@@ -0,0 +1,44 @@
+/**************************************************************************
+ * FILE: parse_arguments.h
+ *
+ * CREATE DATE: 5/22/2004
+ 
+ * AUTHOR: Charles E. Grant
+ 
+ * PROJECT: utilities
+ 
+ * COPYRIGHT: 2004, University of Washington
+ 
+ * DESCRIPTION: Support for parsing of arguments from command line
+ 
+ * CAVEATS:  It is expected that command line arguments will be give in
+ * the form "cmd [option1] [option2] ... [optionn] required1 required2 ..."
+ * Each option will start with '-' and may be followed by a single
+ * option value. Any argument that does not start with '-' and is
+ * not an option value will be taken to be a required argument
+ **************************************************************************/
+#ifndef PARSE_ARGUMENTS
+
+#define PARSE_ARGUMENTS
+
+enum argument_type { FLAG_ARG, INT_ARG, LONG_ARG, DOUBLE_ARG, STRING_ARG };
+enum argument_error { NO_ERROR, UNKNOWN_OPTION, MISSING_VALUE,
+                       INVALID_VALUE, UNKNOWN_REQ_ARG, MISSING_REQ_ARG,
+                        TOO_MANY_REQ_ARGS, TOO_MANY_OPT_ARGS};
+
+typedef struct {
+  const char *name;
+  const char *usage;
+  void *container;
+  enum argument_type type;
+} argument;
+
+int parse_arguments_set_opt(const char * name, const char * usage, 
+                                void * container, enum argument_type type);
+int parse_arguments_set_req(const char * name, const char * usage, 
+                              void * container, enum argument_type type);
+int parse_arguments(int argc, char * argv[], int die_on_error);
+int parse_arguments_get_error(const char ** s);
+char * parse_arguments_get_usage(const char * name);
+
+#endif /*PARSE_ARGUMENTS*/
