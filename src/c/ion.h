0c6198d4e42d28616a533c995e294c0ab07c27c3
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2006-09-21 21:26:15 +0000

    *** empty log message ***

---
+++ src/c/ion.h
@@ -0,0 +1,87 @@
+/**
+ * \file ion.h
+ * $Revision: 1.1 $
+ * \brief Object for representing one ion in an ion_series.
+ *
+ */
+#ifndef ION_H
+#define ION_H
+#include <stdio.h>
+#include <stdlib.h>
+#include "objects.h"
+
+/**
+ * \returns an ION_T object
+ */
+ION_T* new_ion (
+  ION_TYPE_T type,   ///< intensity for the new ion -in 
+  int cleavage_idx, ///< index into the peptide amide bonds of this ion
+  int charge, ///< charge of the ion
+  PEPTIDE_T* peptide ///< location for the new ion -in
+  );
+
+/**
+ * frees A ION_T object
+ */
+void free_ion (
+  ION_T* garbage_ion ///< the ion to free -in
+  );
+
+/**
+ * \returns the location of ION_T object
+ */
+float get_ion_mass(
+  ION_T* working_ion///< return the location of this ion -in 
+  );
+
+
+/**
+ * sets the mass of the ION_T object
+ * while this can be calculated from the PEPTIDE_T, cleavage_idx and
+ * modifications, it allows some optimizations if we allow it to be set
+ * instead
+ */
+void set_ion_mass(
+  ION_T* working_ion, ///<set the m/z location of this ion -out
+  float mass ///< the m/z location -in
+  );
+
+/**
+ * prints the location and fields of ION_T object to the file, in the
+ * following format:
+ *
+ * m/z \\t mass \\t charge \\t ion-series \\t  ...
+ *  peptide-bond-index \\t modifications \n
+ *
+ * Where:
+ *
+ * m/z - is the ion's mass-to-charge
+ * mass - is the ion's (charged) mass
+ * charge - is the ion's charge e.g. 1,2,3
+ * ion-series - is one of (b,y,p)
+ * bond-index - is in [1...n), where n is peptide length
+ * modifications - is one of (none|nh3|h2o)
+ *
+ * if the ion has more than one modification, each will be printed on a
+ * separate line, with the necessary number of tabs to right justify
+ */
+void print_ion(
+  ION_T* ion, ///< print this ion -in
+  FILE* file ///< to this file -in
+  );
+
+/**
+ * Adds the given ION_MODIFICATION to this ion
+ */
+void add_modification(
+  ION_T* ion,///< ion to which to add the modification -mod
+  ION_MODIFICATION_T modification ///< add this modification to the ion -in
+  );
+
+/*
+ * Local Variables:
+ * mode: c
+ * c-basic-offset: 2
+ * End:
+ */
+#endif
---
+++ src/c/ion_series.c
@@ -0,0 +1,47 @@
+/*****************************************************************************
+ * \file ion_series.c
+ * AUTHOR: Chris Park
+ * CREATE DATE: 21 Sep 2006
+ * DESCRIPTION: code to support working with a series of ions
+ * REVISION: $Revision: 1.1 $
+ ****************************************************************************/
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+#include "objects.h"
+#include "ion.h"
+
+#define MAX_IONS 10000
+
+/**
+ * \struct ion_series
+ * \brief An object to represent a series of ions, and organize them!
+ * For which additional data structures will be created as needed 
+ */
+struct ion_series {
+  PEPTIDE_T* peptide; ///< The peptide for this ion series
+  ION_CONSTRAINT_T* constraint; ///< The constraints which the ions in this series obey
+  ION_T* ions[MAX_IONS]; ///< The ions in this series
+  int num_ions; ///< the number of ions in this series
+};
+
+/**
+ * \struct ion_constraint
+ * \brief An object to represent the contraints which the ions in this
+ * series obey.
+ * CHRIS you can add BOOLEANS and other things as needed for implementation
+ * of the predict-peptide-ions executable
+ */
+struct ion_constraint {
+  BOOLEAN_T* use_neutral_losses; ///< A boolean to determine if the ions series should include neutral losses
+}
+
+/*
+ * Local Variables:
+ * mode: c
+ * c-basic-offset: 2
+ * End:
+ */
