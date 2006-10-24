cdfaa1bd29437032d9ed5f7f1f7e375386e5b004
cpark <cpark@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2006-10-24 02:47:47 +0000

    add two python script not done

---
+++ src/python/parse_sqt_file.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python2.4
+# FILE: parse_sqt_file.py
+# AUTHOR: CHRIS PARK
+# CREATE DATE: 20 OCTOBER 2006
+
+"""
+This script parses a .sqt file and returns objects that contain the necessary information
+"""
+
+import os
+import sys
+
+#-------------------
+
+class SqtObject:
+    """an object that contains the spectrum object"""
+
+    def __init__(self, file):
+        self.file = file
+        self.spectrums = []
+
+        if not self.parse(file):
+            print "%s\n" % "failed to create sqt object"
+            self = None;
+            
+    def __parse(self):
+        """ parse the squest file and creates spectrum and peptide objects"""
+        
+        try:
+            sqt_file = open(self.file, "r")
+
+        except IOError:
+            print "%s\n" % "failed to open file"
+            return False
+
+        # might have to split lines
+        # iterate over all line of sqt file
+        for line in sqt_file:
+            if line.startswith('H') || line.startswith('L') : continue
+            elif line.startswith('S'):
+                #parse spectrum
+                fields = line.rstrip('\n').split()
+                id = fields[1].lstrip('0')
+                self.spectrums.append(spectrum((int)id, (int)fields[3], float(fields[6])))
+                
+            elif line.startswith('M'):
+                #parse peptide
+                fields = line.rstrip('\n').split()
+                peptide = Pepetide(float(fields[5]),float(fields[6]), \
+                                   float(fields[3]), fields[9])
+                self.spectrums[len(self.spectrums)-1].addPeptide(peptide)
+        sqt_file.close()
+        return True
+
+#-------------------
+
+class Spectrum:
+    """an object that stores the spectrum"""
+    #the spectrums ID num, charge, observed mass
+    #array of peptide & its score
+    def __init__(self, id, charge, observed_mass):
+        """ initialize the spectrum object """
+        self.fields = {}
+        self.peptides = []
+        self.fields["id"] = id
+        self.fields["charge"] = charge
+        self.fields["observed_mass"] = observed_mass
+        
+    def addPeptide(self, peptide):
+        """add a peptide to the spectrum"""
+        self.peptides.append(peptide)
+
+    def numPeptides(self):
+        """how many peptides are there in the spectrum? """
+        return len(self.peptides)
+
+#-------------------
+        
+class Peptide:
+    """an object that contains info for the peptide"""
+    # xcore, Sp, mass, sequence,  
+    def __init__(self, xcore, sp, mass, sequence):
+        """  fill in the peptide object with all its components """
+        self.components = {}
+        self.components["xcore"] = xcore
+        self.components["sp"] = sp
+        self.components["mass"] = mass
+        self.components["sequence"] = __extractSequence(sequence)
+
+    def __extractSequence(sequence):
+        """ extracts the sequence from the two dots in between """
+        real_sequence = sequence.split('.')
+        return real_sequence[1]
+
+
