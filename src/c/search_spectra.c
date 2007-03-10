93dfc2ff542aa2461932235a63b906f9eb0827a1
cpark <cpark@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2007-03-10 00:14:16 +0000

    going no where

--- src/c/search_spectra.c
+++ src/c/search_spectra.c
@@ -172,8 +172,8 @@ int main(int argc, char** argv){
     spectrum_iterator = new_spectrum_iterator(collection);
     
     //create a generate peptide iterator
-    GENERATE_PEPTIDES_ITERATOR_T* peptide_iterator =  //NULL;//FIXME use neutral_mass, might chage to pick
-      new_generate_peptides_iterator_mutable();
+    //GENERATE_PEPTIDES_ITERATOR_T* peptide_iterator = //FIXME use neutral_mass, might chage to pick
+    //new_generate_peptides_iterator_mutable();
 
 
     //iterate over all spectrum in ms2 file
@@ -196,17 +196,18 @@ int main(int argc, char** argv){
         //print working state
         fprintf(stdout, "# SPECTRUM CHARGE: %d\n", possible_charge_array[charge_index]);
         fprintf(stdout, "# %s\t%s\t%s\t%s\n", "rank", "mass", "sp", "sequence");  
-
-        //DEBUG                
-        //peptide_iterator =  //FIXME use neutral_mass, might chage to pick
-        //new_generate_peptides_iterator_mutable();
+	
+        //DEBUG 
+	//chdir("/home/cpark/crux/bin");
+	//peptide_iterator =  //FIXME use neutral_mass, might chage to pick
+	//new_generate_peptides_iterator_mutable();
 
 
         //get match collection with perlim match collection
         match_collection =
           new_match_collection_spectrum_with_peptide_iterator(spectrum, 
                                                               possible_charge_array[charge_index], 
-                                                              500, perlim_score, peptide_iterator);
+                                                              500, perlim_score);//, peptide_iterator);
         
         
         //later add scoring for main_score here!
@@ -219,23 +220,23 @@ int main(int argc, char** argv){
           match = match_iterator_next(match_iterator);
           print_match(match, stdout, TRUE, SP);
         }
-                
+	
         //free match iterator
         free_match_iterator(match_iterator);
         free_match_collection(match_collection);
-
+	
       }
-
+      
       //THEy are all freed in free_spectrum collection
       //done with all charge states of this spectrum, Free!
       //free_spectrum(spectrum);
     }
-
+    
     free_spectrum_iterator(spectrum_iterator);
     free_spectrum_collection(collection);
     //FIXME!!!
     //free up peptide iterator
-    free_generate_peptides_iterator(peptide_iterator);
+    //free_generate_peptides_iterator(peptide_iterator);
   }
   else{
     char* usage = parse_arguments_get_usage("search_spectra");
