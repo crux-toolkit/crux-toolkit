47fddae1955134b7430973c236f69c37e7e3018d
cpark <cpark@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2007-08-05 22:25:21 +0000

    added score difference code

--- results/paper-figure/score/compare_sequest_score_spectrum.py
+++ results/paper-figure/score/compare_sequest_score_spectrum.py
@@ -19,10 +19,10 @@ from parse_sequest_out_file import SequestObject
 
 #-------------------
 
-def plot_compare_data(result_array, score_type="none"):
+def plot_compare_data(result_array, score_type="none", data_type="none"):
     """ plots the results of two compared as a scattered plot """
     prefix = "Score comparison for " + score_type
-    file_name = "fig-2-" + score_type
+    file_name = "fig-2-" + data_type + "-" + score_type
     
     axis('scaled')
     scatter(result_array[0], result_array[1])
@@ -58,6 +58,36 @@ def plot_compare_data(result_array, score_type="none"):
     savefig(file_name + ".eps")
     savefig(file_name + ".png")
 
+
+#-------------------
+
+##needs some work
+def plot_histogram_data(data_array, score_type="none"):
+    """ plots the results as a histogram """
+    prefix = "Score difference for " + score_type
+    file_name = "fig-2-difference-" + score_type
+    
+    #axis('scaled')
+
+    #plot data as histogram
+    hist(data_array, normed=False, bins=5)
+    
+    if score_type == "sp":
+        title(prefix, size=20)
+        xlim((0, 5))
+        xlabel(score_type, size=15)
+        ylabel("score difference", size=15)
+                
+    elif score_type == "xcorr":
+        title(prefix, size=20)
+        xlim((0, 5))
+        xlabel(score_type, size=15)
+        ylabel("score difference", size=15)
+        
+    savefig(file_name + ".eps")
+    savefig(file_name + ".png")
+
+
 #-------------------
 
 # Process command line options
@@ -92,9 +122,23 @@ for file in files:
 score_type_list = ["sp", "xcorr"]
 
 #the result array for each sequest and CRUX run
-result_array_sp = ([],[])
-result_array_xcorr = ([],[])
+
+#for top scored
+top_result_array_sp = ([],[])
+top_result_array_xcorr = ([],[])
+
+#for randomly selected
+random_result_array_sp = ([],[])
+random_result_array_xcorr = ([],[])
+
+#Sequest/Crux score difference
+diff_sp = []
+diff_xcorr = []
+
 totalCount = 0
+
+#seed the random generator with current time
+random.seed()
 
 #now iterate all dta files and run sequest
 # randomly select one peptide and score against Crux
@@ -129,40 +173,61 @@ for dta_file in dta_file_list:
     if sequest_object == None:
         print "failed to create sequest object from sequest output"
         sys.exit(1)
+    elif len(sequest_object.peptides) == 0:
+        #skip spectrum results with no peptides
+        continue
 
-    # now randomly select a peptide to score against in crux
-    random.seed()
+    #peptides to score
+    peptide_list = []
+
     
-    peptide = random.choice(sequest_object.peptides)
+    # first, randomly select a peptide to score against in crux
+    peptide_list.append(random.choice(sequest_object.peptides))
+    # second, add first ranked xcorr peptide
+    peptide_list.append(sequest_object.peptides[0])
 
-    # for each score type compare
-    for score_type in score_type_list:
-        #debug
-        #print peptide.components
+    #now iterate over peptide (random and top peptide)
+    for idx, peptide in enumerate(peptide_list):
 
-        (exit_code, result_crux) = \
-                    commands.getstatusoutput("score_peptide_spectrum " + \
-                                             "--charge " + `charge` + " --score-type " + score_type + " " + \
-                                             peptide.components["sequence"] + " " + \
-                                             `scan_number` + " " + \
-                                             ms2_file 
-                                             ) #add in parameters
-
-        if exit_code == "1":
-            print "failed to run score_peptide_spectrum"
-            sys.exit(1)
-
-        #print result_crux
+        #Random peptide?
+        if(idx == 0):
+            result_array_sp = random_result_array_sp
+            result_array_xcorr = random_result_array_xcorr
+        else: #top peptide?
+            result_array_sp = top_result_array_sp
+            result_array_xcorr = top_result_array_xcorr
         
-        result_crux = result_crux.split(': ')
+        # for each score type compare
+        for score_type in score_type_list:
+            #debug
+            #print peptide.components
 
-        if score_type == "sp":
-            result_array_sp[0].append(peptide.components[score_type])
-            result_array_sp[1].append(float(result_crux[1]))
-        elif score_type == "xcorr":
-            result_array_xcorr[0].append(peptide.components[score_type])
-            result_array_xcorr[1].append(float(result_crux[1]))
-            
+            (exit_code, result_crux) = \
+                        commands.getstatusoutput("score_peptide_spectrum " + \
+                                                 "--charge " + `charge` + " --score-type " + score_type + " " + \
+                                                 peptide.components["sequence"] + " " + \
+                                                 `scan_number` + " " + \
+                                                 ms2_file 
+                                                 ) #add in parameters
+
+            if exit_code == "1":
+                print "failed to run score_peptide_spectrum"
+                sys.exit(1)
+
+            #print result_crux
+        
+            result_crux = result_crux.split(': ')
+
+            if score_type == "sp":
+                #store both Sequest, Crux scores for the peptide
+                result_array_sp[0].append(peptide.components[score_type])
+                result_array_sp[1].append(float(result_crux[1]))
+                #store the difference in the score values
+                diff_sp.append(  abs(float(result_crux[1]) - peptide.components[score_type])  )
+            elif score_type == "xcorr":
+                result_array_xcorr[0].append(peptide.components[score_type])
+                result_array_xcorr[1].append(float(result_crux[1]))
+                diff_xcorr.append( abs(float(result_crux[1]) - peptide.components[score_type]) )
         
     if totalCount % 100 == 0:
         print "totalCount: %d" % totalCount
@@ -173,5 +238,16 @@ for dta_file in dta_file_list:
 
 # plot the data
 print "generating figure"
-plot_compare_data(result_array_sp, "sp")
-plot_compare_data(result_array_xcorr, "xcorr")
+
+# first plot top ranked peptide comparison
+plot_compare_data(top_result_array_sp, "sp", "top")
+plot_compare_data(top_result_array_xcorr, "xcorr", "top")
+
+# second, plot randomly picked peptide comparison
+plot_compare_data(random_result_array_sp, "sp", "random")
+plot_compare_data(random_result_array_xcorr, "xcorr", "random")
+
+# third, plot the score difference of SP, and Xcorr for each peptide between Sequest and Crux
+# currently, the data is for both top and random peptides, thus twice much points compared to the other plots
+plot_histogram_data(diff_sp, "sp")
+plot_histogram_data(diff_xcorr, "xcorr")
