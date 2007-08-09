2089f09bdbc2cae671bef53115eccc3b15ebf7c4
cpark <cpark@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2007-08-09 00:06:03 +0000

    added sample_spectra_from_ms2.py

--- src/python/parse_sqt_file.py
+++ src/python/parse_sqt_file.py
@@ -11,6 +11,115 @@ import os
 import sys
 
 #-------------------
+class CruxObject:
+    """an object that contains the crux result fields"""
+
+    def __init__(self, file, mode="sp", fromFile=True):
+        self.file = file
+        self.mode = mode
+        self.spectrums = []
+
+        if fromFile:
+            if not self.__parse():
+                print "%s\n" % "failed to create sqt object"
+        else:
+            if not self.__parse_string():
+                print "%s\n" % "failed to create sqt object"
+                
+    def __parse(self):
+        """ starts from filename, parse the crux result file and creates spectrum and peptide objects"""
+        try:
+            crux_file = open(self.file, "r")
+            
+        except IOError:
+            print "%s\n" % "failed to open file"
+            return False
+
+        if not self.__parse_crux_object(crux_file):
+            crux_file.close()
+            return False
+
+        crux_file.close()
+        return True
+    
+    def __parse_string(self):
+        """ starts from string, parse the crux result file and creates spectrum and peptide objects"""
+        return self.__parse_crux_object_2(self.file)
+
+    def __parse_crux_object(self, crux_file):
+        count = 0
+        for line in crux_file:
+            # new spectrum, create a spectrum object
+            if line.startswith('#'):
+                fields = line.rstrip('\n').split()
+                if fields[2] == "SCAN":
+                    scan_num = int(fields[4])
+                elif fields[2] == "PRECURSOR":
+                    mz = float(fields[4])
+                elif fields[2] == "CHARGE:":
+                    charge = int(fields[3])
+                    #create spectrum object
+                    self.spectrums.append(Spectrum(scan_num, charge, mz))
+                    # peptide objects of the spectrum
+                    count += 1
+                    print "%s: %d" % ("spectrum added", count)
+            elif line.startswith('P'):
+                fields = line.rstrip('\n').split()
+                # xcorr output format
+                if self.mode == "xcorr":
+                    peptide =  Peptide(int(fields[1]), float(fields[4]),float(fields[5]), \
+                                       float(fields[3]), fields[7], int(fields[2]))
+                    # sp output format
+                elif self.mode == "sp":
+                    peptide =  Peptide(None, None, float(fields[3]), float(fields[2]), \
+                                       fields[5], int(fields[1]))
+                else:
+                    print "%s\n" % "In correct mode, must be sp or xcorr"
+                    return False
+                # add peptide to spectrum
+                self.spectrums[len(self.spectrums)-1].addPeptide(peptide)
+        return True
+
+    def __parse_crux_object_2(self, crux_file):
+        count = 0
+        lines = crux_file.split('\n')
+        
+        for line in lines:
+            # new spectrum, create a spectrum object
+            if line.startswith('#'):
+                fields = line.split()
+                if fields[2] == "SCAN":
+                    scan_num = int(fields[4])
+                elif fields[2] == "PRECURSOR":
+                    mz = float(fields[4])
+                elif fields[2] == "CHARGE:":
+                    charge = int(fields[3])
+                    #create spectrum object
+                    self.spectrums.append(Spectrum(scan_num, charge, mz))
+                    # peptide objects of the spectrum
+                    count += 1
+                    print "%s: %d" % ("spectrum added", count)
+            elif line.startswith('P'):
+                fields = line.split()
+                # Peptide object arguments(xcorr_rank, xcore, sp, mass, sequence, sp_rank=None)
+                if self.mode == "xcorr":
+                    peptide =  Peptide(int(fields[1]), float(fields[4]),float(fields[5]), \
+                                       float(fields[3]), fields[6], int(fields[2]))
+                    # sp output format
+                elif self.mode == "sp":
+                    peptide =  Peptide(None, None, float(fields[5]), float(fields[3]), \
+                                       fields[6], int(fields[2]))
+                elif self.mode == "logp_exp_sp":
+                    peptide =  Peptide(None, None, float(fields[5]), float(fields[3]), \
+                                       fields[6], int(fields[2]))
+                else:
+                    print "%s\n" % "In correct mode, must be sp or xcorr"
+                    return False
+                # add peptide to spectrum
+                self.spectrums[len(self.spectrums)-1].addPeptide(peptide)
+        return True
+
+#-------------------
 
 class SqtObject:
     """an object that contains the spectrum object"""
@@ -21,7 +130,7 @@ class SqtObject:
 
         if not self.__parse():
             print "%s\n" % "failed to create sqt object"
-            self = None;
+            self = None
             
     def __parse(self):
         """ parse the squest file and creates spectrum and peptide objects"""
@@ -56,13 +165,13 @@ class SqtObject:
 
 class Spectrum:
     """an object that stores the spectrum"""
-    #the spectrums ID num, charge, observed mass
+    #the spectrums scan num, charge, observed mass
     #array of peptide & its score
-    def __init__(self, id, charge, observed_mass):
+    def __init__(self, scan, charge, observed_mass):
         """ initialize the spectrum object """
         self.fields = {}
         self.peptides = []
-        self.fields["id"] = id
+        self.fields["scan"] = scan
         self.fields["charge"] = charge
         self.fields["observed_mass"] = observed_mass
         
@@ -79,11 +188,11 @@ class Spectrum:
 class Peptide:
     """an object that contains info for the peptide"""
     # xcore, Sp, mass, sequence,  
-    def __init__(self, xcore_rank, xcore, sp, mass, sequence, sp_rank=None):
+    def __init__(self, xcorr_rank, xcore, sp, mass, sequence, sp_rank=None):
         """  fill in the peptide object with all its components """
         self.components = {}
-        self.components["xcore_rank"] = xcore_rank
-        self.components["xcore"] = xcore
+        self.components["xcorr_rank"] = xcorr_rank
+        self.components["xcorr"] = xcore
         self.components["sp"] = sp
         self.components["mass"] = mass
         self.components["sequence"] = self.__extractSequence(sequence)
@@ -92,6 +201,7 @@ class Peptide:
     def __extractSequence(self, sequence):
         """ extracts the sequence from the two dots in between """
         real_sequence = sequence.split('.')
-        return real_sequence[1]
-
+        if len(real_sequence) == 3:
+            return real_sequence[1]
+        return real_sequence[0]
 
