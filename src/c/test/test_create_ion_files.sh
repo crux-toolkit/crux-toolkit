d1367079d10e62a258a56e873afee3c904b972a6
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2007-08-15 22:45:34 +0000

    Fixed segfault in create_ion_files. yay!

--- src/c/test/test_create_ion_files.sh
+++ src/c/test/test_create_ion_files.sh
@@ -1,7 +1,7 @@
- cd ../
+# cd ../
 # make clean
- rm -f ../../bin/create_ion_files
- make
- cd test
+# rm -f ../../bin/create_ion_files
+# make
+# cd test
  rm -fr output
 ../../../bin/create_ion_files peptides.txt 13002 test-3.ms2 output --verbosity 100
