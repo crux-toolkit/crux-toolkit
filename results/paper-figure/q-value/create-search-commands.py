7512197ef79200de77f9d457d1b838c99c77b8ae
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2007-08-30 18:52:49 +0000

    *** empty log message ***

--- results/paper-figure/q-value/create-search-commands.py
+++ results/paper-figure/q-value/create-search-commands.py
@@ -1,21 +1,21 @@
 import sys
 import os
 # simple python script to submit to the cluster
-MATCH_SEARCH_PATH="/nfs/gs/home/aklammer/crux/bin/match_search"
+MATCH_SEARCH_PATH="/nfs/gs/home/aklammer/crux/results/paper-figure/q-value/match_search"
 
 if len(sys.argv) < 3:
-  raise SystemExit, "%s: <mass-range-file> [mass-analysis args]*" % sys.argv[0]
+  raise SystemExit, "%s: <mass-range-file> [mass-search args]*" % sys.argv[0]
 
 mass_range_file = sys.argv[1]
-match_analysis_args = sys.argv[2:]
+match_search_args = sys.argv[2:]
 fh = open(mass_range_file)
 cwd = os.getcwd()
 for line in fh:
   start, end = line.rstrip("\n").split()
-  cmdFile = os.getcwd() + os.sep + "match_analysis_%s_%s" % (start, end)
+  cmdFile = os.getcwd() + os.sep + "match_search_%s_%s" % (start, end)
   outFh = open(cmdFile, "w")
   cmd = "%s --spectrum-min-mass %.3f --spectrum-max-mass %.3f %s \n" \
-     % (MATCH_SEARCH_PATH, float(start), float(end), " ".join(match_analysis_args))
+     % (MATCH_SEARCH_PATH, float(start), float(end), " ".join(match_search_args))
   outFh.write(cmd)
   outFh.close()
   clusterCmd = "qsub -o %s -e %s %s" % (cwd, cwd, cmdFile)
