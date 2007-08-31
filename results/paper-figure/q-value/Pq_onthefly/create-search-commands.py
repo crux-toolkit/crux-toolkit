8ceaa056e6776ff46d5510d41428d309ea920a3d
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2007-08-31 22:48:29 +0000

    *** empty log message ***

---
+++ results/paper-figure/q-value/Pq_onthefly/create-search-commands.py
@@ -0,0 +1,26 @@
+import sys
+import os
+# simple python script to submit to the cluster
+MATCH_SEARCH_PATH=os.getcwd() + os.sep + ".." + os.sep + "match_search"
+
+if len(sys.argv) < 3:
+  raise SystemExit, "%s: <mass-range-file> [mass-search args]*" % sys.argv[0]
+
+mass_range_file = sys.argv[1]
+fasta_file = sys.argv[3]
+base = os.path.basename(fasta_file).split(".")[0]
+match_search_args = sys.argv[2:]
+fh = open(mass_range_file)
+cwd = os.getcwd()
+for line in fh:
+  start, end = line.rstrip("\n").split()
+  tag = "%s_%s_%s" % (start, end, base)
+  cmdFile = os.getcwd() + os.sep + "match_search_%s" % tag
+  outFh = open(cmdFile, "w")
+  cmd = "cd %s; %s --output-mode sqt --sqt-output-file %s.sqt --number-decoy-set 0 --spectrum-min-mass %.3f --spectrum-max-mass %.3f %s \n" \
+     % (os.getcwd(), MATCH_SEARCH_PATH, tag, float(start), float(end), " ".join(match_search_args))
+  outFh.write(cmd)
+  outFh.close()
+  clusterCmd = "qsub -o %s -e %s %s" % (cwd, cwd, cmdFile)
+  print clusterCmd
+
