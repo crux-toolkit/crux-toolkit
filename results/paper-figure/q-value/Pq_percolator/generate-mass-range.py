846c36a1af8969541ef0064df396a3f1c82fdf82
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2007-08-31 00:21:42 +0000

    *** empty log message ***

--- results/paper-figure/q-value/Pq_percolator/generate-mass-range.py
+++ results/paper-figure/q-value/Pq_percolator/generate-mass-range.py
@@ -1,5 +1,5 @@
-min_mass = 0.00
-max_mass = 7200.0
-step = 200.0 
+min_mass = 400.00
+max_mass = 1400.0
+step = 50.0 
 for i in range(int(min_mass),int(max_mass),int(step)):
   print "%.2f\t%.2f" % (i, i+step)
