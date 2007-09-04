4d4d143fa287460255d8bd598b6cc5e3bf3acbe4
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2007-09-04 19:56:46 +0000

    *** empty log message ***

--- results/paper-figure/q-value/plot.py
+++ results/paper-figure/q-value/plot.py
@@ -1,10 +1,15 @@
 import plotFuncs
 import pylab
 
-plotFuncs.plotXYsFromFile("Pq_onthefly/pq")
-plotFuncs.plotXYsFromFile("Pq_static/pq")
-plotFuncs.plotXYsFromFile("Pq_sequest/pq")
-plotFuncs.plotXYsFromFile("Pq_percolator/pq")
-pylab.savefig("p-value.eps")
-pylab.savefig("p-value.png")
-pylab.show()
+plotFuncs.plotXYsFromFile("Pq_onthefly/pq", label="On the fly null", style="k-")
+plotFuncs.plotXYsFromFile("Pq_static/pq", label="Static null", style="g-")
+plotFuncs.plotXYsFromFile("Pq_sequest/pq", label="Sequest", style="m-")
+# plotFuncs.plotXYsFromFile("Pq_percolator/pq", label="Percolator", style="b-")
+# plotFuncs.plotXYsFromFile("Pq_percolator-advanced/pq", label="Percolator extended", style="o-")
+pylab.legend()
+pylab.xlabel("q-value")
+pylab.xlim(0.0,0.10)
+pylab.ylabel("positives")
+pylab.ylim(0.0,400.0)
+pylab.savefig("q-value.eps")
+pylab.savefig("q-value.png")
