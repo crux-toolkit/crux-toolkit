e84dc5a5b0c26edacd75820d5c597e908cbd6d84
(no author) <(no author)@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2007-10-18 21:31:11 +0000

    This commit was manufactured by cvs2svn to create tag 'before_BF_changes'.

--- results/paper-figure/q-value/Pq_percolator/test-perc.sh
+++
@@ -1,7 +0,0 @@
-# (~/bin/percolator.n001  -g match_analysis.mtx match_analysis.labels > perc-gist.out ) >& perc-gist.err
-# (~/bin/percolator.n001  all-t.sqt all-d.sqt > perc-sqt.out) >& perc-sqt.err
-for i in `cat gist-header | cut -f2-`;{
-  echo $i
-  roc -column 0 -score $i match_analysis.features
-}
-
