78db88a4ff758151a3a9bfdd9a4f6ffc7cb59367
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2007-10-25 20:59:55 +0000

    *** empty log message ***

--- results/paper-figure/q-value/Pq_percolator/create-search-commands.py
+++ results/paper-figure/q-value/Pq_percolator/create-search-commands.py
@@ -3,7 +3,7 @@ import os
 
 # simple python script to submit to the cluster
 
-MATCH_SEARCH_PATH = "/nfs/gs/home/aklammer/crux/results/paper-figure/q-value/match_search"
+MATCH_SEARCH_PATH = "/nfs/gs/home/aklammer/crux/results/paper-figure/q-value/Pq_percolator/match_search"
 
 if len(sys.argv) < 3:
   raise SystemExit, "%s: <mass-range-file> [mass-search args]*" % sys.argv[0]
@@ -18,13 +18,13 @@ for line in fh:
   sqtFile = "match_search_%s_%s_target.sqt" % (start, end)
   decoySqtFile = "match_search_%s_%s_decoy.sqt" % (start, end)
   outFh = open(cmdFile, "w")
-  cmd = ("cd %s; %s --output-mode all --sqt-output-file %s " +    \
-        "--decoy-sqt-output-file %s --spectrum-min-mass %.3f " +   \
+  cmd = ("cd %s; %s " +    \
+        "--spectrum-min-mass %.3f " +   \
         "--spectrum-max-mass %.3f %s \n")                         \
-        % (os.getcwd(), MATCH_SEARCH_PATH, sqtFile, decoySqtFile, \
+        % (os.getcwd(), MATCH_SEARCH_PATH, \
           float(start), float(end), " ".join(match_search_args))
   outFh.write(cmd)
   outFh.close()
-  clusterCmd = "qsub -o %s -e %s %s" % (cwd, cwd, cmdFile)
+  clusterCmd = "qsub -cwd -o %s -e %s %s" % (cwd, cwd, cmdFile)
   print clusterCmd
 
--- results/paper-figure/q-value/Pq_percolator/final-analysis.sh
+++ results/paper-figure/q-value/Pq_percolator/final-analysis.sh
@@ -7,7 +7,9 @@
 export OUTPUT=`basename ${PREFIX}`.anal
 
 #------------
-# (../match_analysis `pwd` $FASTA --feature-file match_analysis.features --parameter-file $PARAM_FILE --algorithm percolator --verbosity 100 > $OUTPUT  )>& error
+# ./match_analysis `pwd` $FASTA --verbosity 30 --feature-file match_analysis.features --parameter-file $PARAM_FILE --algorithm percolator
+(./match_analysis `pwd` $FASTA --feature-file match_analysis.features --parameter-file $PARAM_FILE --algorithm percolator > $OUTPUT  )>& error
+exit
 cut -f1,5 $OUTPUT | cut -f2 -d' ' > pq.xy.tmp
 cut -f2 pq.xy.tmp > 1.tmp
 cut -f1 pq.xy.tmp > 2.tmp
