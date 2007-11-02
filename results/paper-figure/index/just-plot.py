12d22b899a398818554c24a6583911b2a2e33bd2
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2007-11-02 19:31:43 +0000

    *** empty log message ***

--- results/paper-figure/index/just-plot.py
+++ results/paper-figure/index/just-plot.py
@@ -1,11 +1,20 @@
 import plotFuncs, pylab
-plotFuncs.plotLogLogFromFile("crux-no-index", style="k-o", label="Crux (w/o index)")
-plotFuncs.plotLogLogFromFile("sequest", style="m-o", label="Sequest")
-plotFuncs.plotLogLogFromFile("crux-fast", style="g-o", label="Crux (w/ index)")
+plotFuncs.plotLogLogFromFile("crux-no-index-human.fasta", style="k-o",
+    label="Crux w/o index (human)")
+plotFuncs.plotLogLogFromFile("crux-no-index-human-dummy.fasta",
+    style="k--o", label="Crux w/o index (yeast)")
+plotFuncs.plotLogLogFromFile("sequest-human.fasta", style="m-s",
+    label="Sequest (human)")
+plotFuncs.plotLogLogFromFile("sequest-human-dummy.fasta", style="m--s",
+    label="Sequest (yeast)")
+plotFuncs.plotLogLogFromFile("crux-fast-human.fasta", style="g-^", 
+    label="Crux (w/ index) human")
+plotFuncs.plotLogLogFromFile("crux-fast-human-dummy.fasta", style="g--^", 
+    label="Crux (w/ index) yeast")
 pylab.legend(loc="lower right")
 pylab.xlabel("Mass window (Da)")
 pylab.xlim(0.05, 5)
-pylab.ylim(0.7, 10000)
+pylab.ylim(0.1, 50000)
 pylab.ylabel("Runtime for 1000 spectra (s)")
 pylab.savefig("indexing.eps")
 pylab.savefig("indexing.png")
