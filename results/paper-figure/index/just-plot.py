a176038fdc0738958da203480b1b755de987ca4b
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2007-11-04 05:47:40 +0000

    *** empty log message ***

--- results/paper-figure/index/just-plot.py
+++ results/paper-figure/index/just-plot.py
@@ -1,20 +1,29 @@
 import plotFuncs, pylab
 plotFuncs.plotLogLogFromFile("crux-no-index-human.fasta", style="k-o",
-    label="Crux w/o index (human)")
-plotFuncs.plotLogLogFromFile("crux-no-index-human-dummy.fasta",
-    style="k--o", label="Crux w/o index (yeast)")
+    label="Crux (w/o index)")
 plotFuncs.plotLogLogFromFile("sequest-human.fasta", style="m-s",
-    label="Sequest (human)")
-plotFuncs.plotLogLogFromFile("sequest-human-dummy.fasta", style="m--s",
-    label="Sequest (yeast)")
+    label="Sequest")
 plotFuncs.plotLogLogFromFile("crux-fast-human.fasta", style="g-^", 
-    label="Crux (w/ index) human")
-plotFuncs.plotLogLogFromFile("crux-fast-human-dummy.fasta", style="g--^", 
-    label="Crux (w/ index) yeast")
+    label="Crux (w/ index)")
 pylab.legend(loc="lower right")
 pylab.xlabel("Mass window (Da)")
 pylab.xlim(0.05, 5)
 pylab.ylim(0.1, 50000)
-pylab.ylabel("Runtime for 1000 spectra (s)")
-pylab.savefig("indexing.eps")
-pylab.savefig("indexing.png")
+pylab.ylabel("Runtime for 100 spectra (s)")
+pylab.savefig("indexing-human.eps")
+pylab.savefig("indexing-human.png")
+pylab.clf()
+
+plotFuncs.plotLogLogFromFile("crux-no-index-yeast.fasta", style="k-o",
+    label="Crux (w/o index)")
+plotFuncs.plotLogLogFromFile("sequest-yeast.fasta", style="m-s",
+    label="Sequest")
+plotFuncs.plotLogLogFromFile("crux-fast-yeast.fasta", style="g-^", 
+    label="Crux (w/ index)")
+pylab.legend(loc="lower right")
+pylab.xlabel("Mass window (Da)")
+pylab.xlim(0.05, 5)
+pylab.ylim(0.1, 50000)
+pylab.ylabel("Runtime for 100 spectra (s)")
+pylab.savefig("indexing-yeast.eps")
+pylab.savefig("indexing-yeast.png")
