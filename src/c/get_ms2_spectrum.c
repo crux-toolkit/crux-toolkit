d809df87f35ea30c76f1b5eb25ee87b0d2f757d4
frewen <frewen@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2007-12-18 20:18:44 +0000

    removed old code from get_ms2_spectrum.c

--- src/c/get_ms2_spectrum.c
+++ src/c/get_ms2_spectrum.c
@@ -32,17 +32,7 @@ int main(int argc, char** argv){
   FILE* output_file; ///< output file name
   //OUTPUT_WRITE_MODE_T file_flag;
   BOOLEAN_T options = FALSE; ///< do we want options?
-  /*
-  char* USAGE = 
-    "USAGE: get_ms2_spectrum"
-    "\t<scan number>\n"
-    "\t<input filename>\n"
-    "\t<output filename>\n"
-    "\t[--stats]\n"
-    "SYNOPSIS: get_ms2_spectrum scan_number input_file input_file [--stats]\n"  
-    "OPTIONS: --stats\n"
-    "\tOutput some summary statistics for the particular spectrum to STDOUT\n"; 
-  */
+
   /* Define optional command line arguments */
   int num_options = NUM_MS2_OPTIONS;
   char* option_list[NUM_MS2_OPTIONS] = { "stats", "verbosity" }; //out-file
@@ -70,39 +60,6 @@ int main(int argc, char** argv){
   /* Set verbosity */
   set_verbosity_level(get_int_parameter("verbosity"));
 
-  /*
-  // check command line argument count
-  if (argc != 4) {
-    if(argc == 5 && 
-       strcmp(argv[4], "--stats")==0){
-      options = TRUE;      
-    }
-    else{
-      fprintf (stderr, "%s", USAGE);
-      exit(1);
-    }
-  }
-  
-  // check if first argument is type int for scan number
-  if (sscanf(argv[1], "%d", &scan_num) != 1) {
-    fprintf (stderr, "ERROR:second arguement must be type int\n");
-    fprintf (stderr, "%s", USAGE);
-    exit(1);
-  }
-
-  // check if the input file exist
-  if(access(argv[2], F_OK)){
-    fprintf(stderr,"input file:\"%s\" could not be opened\n", argv[2]);
-    exit(1);
-  }
-  
-  // check if the output file doesn't aleady exist
-  if(!access(argv[3], F_OK)){
-    fprintf(stderr,"output file:\"%s\" already exist\n", argv[3]);
-    exit(1);
-  }
-  */
-
   /* Get arguments */
   scan_number = get_int_parameter("scan number");
   char* ms2_filename = get_string_parameter_pointer("ms2 file");
@@ -112,9 +69,7 @@ int main(int argc, char** argv){
   options = get_boolean_parameter("stats");
   //file_flag = get_output_mode_parameter("out-file");
 
-
   /* read input file */
-  //  collection = new_spectrum_collection(argv[2]);
   if( access(ms2_filename, F_OK)){
     carp(CARP_FATAL, "Could not read from ms2 file '%s'", ms2_filename);
     exit(1);
@@ -126,15 +81,12 @@ int main(int argc, char** argv){
   spectrum_found = get_spectrum_collection_spectrum(collection, 
 							  scan_number, 
 							  spectrum);
-//  if(get_spectrum_collection_spectrum(collection, scan_number, spectrum)){ 
   if( !spectrum_found ){
     carp(CARP_FATAL, "Could not find scan number %i", scan_number);
     exit(1);
   }
 
-  //    printf("Found a spectrum with correct scan number\n");
   carp(CARP_INFO, "Found a spectrum with scan number %i", scan_number);
-  //    output_file = fopen(argv[3], "w");
 
   if( !access(output_filename, F_OK)){//&& file_flag != FILE_REPLACE
     carp(CARP_FATAL, "The output file '%s' already exists.  " \
@@ -154,25 +106,13 @@ int main(int argc, char** argv){
 
   print_spectrum(spectrum, output_file);
   fclose(output_file);
-  //spectrum_found = TRUE;
 
-/*  else{
-    fprintf(stderr,
-            "Warning:\n" 
-            "The ms2 file %s does not contain the spectrum with scan number %d.\n", 
-            argv[2], 
-            scan_number);
-    spectrum_found = FALSE;
-  }
-*/
   int charge_state_index = 0; 
   int charge_state_num = get_spectrum_num_possible_z(spectrum);
   int* possible_z_array = get_spectrum_possible_z(spectrum);
   int possible_z;
   
-  // print only if found spectrum and with option flag
   /* Print stats if requested */
-  //  if(spectrum_found && options){
   if(options){
     printf("Scan number: %i\n", scan_number);
     printf("Precursor m/z:%.2f\n", get_spectrum_precursor_mz(spectrum));
@@ -199,14 +139,6 @@ int main(int argc, char** argv){
   free_spectrum(spectrum);
   free_spectrum_collection(collection);
   
-  // failed to find spectrum
-  /*
-  if(!spectrum_found){
-    return(1);
-  }
-  */
-  // exit success
-
   carp(CARP_INFO, "crux-get-ms2-spectrum finished.");
   return(0);
 }
