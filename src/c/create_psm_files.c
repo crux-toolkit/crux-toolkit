84e5533dc7d5954a81de8a46c6e06aa26a3cad44
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2008-01-03 19:27:52 +0000

    *** empty log message ***

--- src/c/create_psm_files.c
+++ src/c/create_psm_files.c
@@ -3,7 +3,7 @@
  * AUTHOR: Aaron Klammer
  * CREATE DATE: 8/8 2007
  * DESCRIPTION: Creates files describing ion series, for input to GMTK.
- * REVISION: $Revision: 1.7 $
+ * REVISION: $Revision: 1.8 $
  ****************************************************************************/
 #include <math.h>
 #include <stdlib.h>
@@ -42,167 +42,120 @@ void wrong_command(char* arg, char* comment){
   exit(1);
 }
 
+#define PSM_NUM_OPTIONS 4
+#define PSM_NUM_ARGUMENTS 5
+
 int main(int argc, char** argv){
 
   // required variables
-  char* ms2_file = NULL;
-  int scan_num = 0;
-  char* peptide_file_name = NULL;
-  char* output_directory = NULL;
+  char* ms2_file;
+  int scan_num;
+  char* peptide_file_name;
+  char* output_directory;
 
   // optional variables
-  int charge = 2;
-  int starting_sentence_idx = 0;
-  char* parameter_file = NULL;
-  char* model_type = NULL;
-  int  verbosity = CARP_ERROR;
-
-  // parsing variables
-  int result = 0;
-  char * error_message; 
+  int charge;
+  int starting_sentence_idx;
+  char* model_type;
 
   /* Define optional command line arguments */ 
-  parse_arguments_set_opt(
-    "charge", 
-    "The peptide charge. 1|2|3",
-    (void *) &charge, 
-    INT_ARG);
+  int num_options = PSM_NUM_OPTIONS;
+  char* option_list[PSM_NUM_OPTIONS] = { 
+    "verbosity",
+    "parameter-file", 
+    "spectrum-charge",
+    "starting-sentence-idx"
+  };
+
+  /* Define required command line arguments */ 
+  int num_arguments = PSM_NUM_ARGUMENTS;
+  char* argument_list[PSM_NUM_ARGUMENTS] = { 
+    "peptide-file-name",
+    "scan number",
+    "ms2 file",
+    "output-dir",
+    "model-type"
+  };
+
+  set_verbosity_level(CARP_ERROR);
+  carp(CARP_DETAILED_DEBUG, "Creating ion files");
+
+
+  initialize_parameters();
+  select_cmd_line_options(option_list, num_options);
+  select_cmd_line_arguments(argument_list, num_arguments);
+
+  parse_cmd_line_into_params_hash(argc, argv, "create_psm_files");
+
+  // parsed arguments
+  ms2_file = get_string_parameter("ms2 file");
+  scan_num = get_int_parameter("scan number");
+  peptide_file_name = get_string_parameter("peptide-file-name");
+  output_directory = get_string_parameter("output-dir");
+  model_type = get_string_parameter("model-type");
+  charge = get_int_parameter("spectrum-charge");
+  starting_sentence_idx = get_int_parameter("starting-sentence-idx");
+
+  SPECTRUM_T* spectrum = NULL;
+  SPECTRUM_COLLECTION_T* collection = NULL;
+ 
+  // read ms2 file
+  carp(CARP_INFO, "Reading ms2 file %s", ms2_file);
+  collection = new_spectrum_collection(ms2_file);
+  spectrum = allocate_spectrum();
   
-  parse_arguments_set_opt(
-    "parameter-file",
-    "The crux parameter file to parse parameter from.",
-    (void *) &parameter_file,
-    STRING_ARG);
-
-  parse_arguments_set_opt(
-    "verbosity", 
-    "Specify the verbosity of the current processes from 0-100.",
-    (void *) &verbosity, 
-    INT_ARG);
-
-  parse_arguments_set_opt(
-    "starting-sentence-idx", 
-    "The first sentence idx (Default = 0).",
-    (void *) &starting_sentence_idx,
-    INT_ARG);
-
- /* Define required command line arguments */
-  parse_arguments_set_req(
-    "peptide-file-name", 
-    "A file containing the peptide sequences for which to create ions files.", 
-    (void *) &peptide_file_name, 
-    STRING_ARG);
-
-  parse_arguments_set_req(
-    "scan-number", 
-    "The scan number for the MS-MS spectrum to extract from the ms2 file.",
-    (void *) &scan_num, INT_ARG);
-
-  parse_arguments_set_req(
-    "ms2-filename", 
-    "A file containing multiple MS-MS spectra in .ms2 format.",
-    (void *) &ms2_file,
-    STRING_ARG);
-
-	parse_arguments_set_req(
-    "output-dir", 
-    "A directory in which to place the ion files.",
-    (void *) &output_directory,
-    STRING_ARG);
-
-  parse_arguments_set_req(
-    "model-type", 
-    "The kind of model to output files for (paired|single)",
-    (void *) &model_type,
-    STRING_ARG);
-
-  /* Parse the command line */
-  if (parse_arguments(argc, argv, 0)) {
-
-    // parsed arguments
-    SPECTRUM_T* spectrum = NULL;
-    SPECTRUM_COLLECTION_T* collection = NULL;
-    
-    // set verbosity
-    if(CARP_FATAL <= verbosity && verbosity <= CARP_MAX){
-      set_verbosity_level(verbosity);
-    }
-    else{
-      wrong_command("verbosity", "verbosity level must be between 0-100");
-    }
-
-    // parse and update parameters
-    parse_update_parameters(parameter_file);
-    
-    // parameters now can't be changed
-    parameters_confirmed();
-	
-    // read ms2 file
-    carp(CARP_INFO, "Reading ms2 file %s", ms2_file);
-    collection = new_spectrum_collection(ms2_file);
-    spectrum = allocate_spectrum();
-    
-    // search for spectrum with correct scan number
-    carp(CARP_INFO, "Retrieving spectrum %i", scan_num);
-    if(!get_spectrum_collection_spectrum(collection, scan_num, spectrum)){
-      carp(CARP_ERROR, "Failed to find spectrum with scan_num: %d", scan_num);
-      free_spectrum_collection(collection);
-      free_spectrum(spectrum);
-      exit(1);
-    }
-
-		// prepare the spectrum 
-    carp(CARP_INFO, "Normalizing spectrum %i", scan_num);
-		sum_normalize_spectrum(spectrum);
-
-    carp(CARP_INFO, "Ranking spectrum peaks %i", scan_num);
-    spectrum_rank_peaks(spectrum); 
-
-		// parse the peptides
-		int num_lines;
-    carp(CARP_INFO, "Parsing peptides from %s", peptide_file_name);
-		char** peptides = parse_file(peptide_file_name, MAX_PEPTIDES, &num_lines);
-				
-    carp(CARP_INFO, "Creating and outputting ions");
-
-    // output GMTK peptide ion files
-    // PAIRED add output_psm_files_paired and change this to 
-    // output_psm_files_single
-    BOOLEAN_T return_status = FALSE;
-    if (strcmp(model_type, "single") == 0){
-      return_status = output_psm_files_single(
-          output_directory, spectrum, peptides, num_lines, 
-          charge, starting_sentence_idx);
-    } else if (strcmp(model_type, "paired") == 0){
-      return_status = output_psm_files_paired(
-          output_directory, spectrum, peptides, num_lines, 
-          charge, starting_sentence_idx);
-    }
-
-    if (return_status == FALSE){
-      carp(CARP_FATAL, "Failed to create ion files for: %s %i %s.", 
-         ms2_file, scan_num, peptide_file_name);
-    } else {
-      carp(CARP_INFO, "Done outputting files.");
-    }
-
-    // free heap 
-		int peptide_idx = 0;
-    for (peptide_idx=0; peptide_idx < num_lines; peptide_idx++){
-      free(peptides[peptide_idx]);
-    }
-    free(peptides);
+  // search for spectrum with correct scan number
+  carp(CARP_INFO, "Retrieving spectrum %i", scan_num);
+  if(!get_spectrum_collection_spectrum(collection, scan_num, spectrum)){
+    carp(CARP_ERROR, "Failed to find spectrum with scan_num: %d", scan_num);
     free_spectrum_collection(collection);
     free_spectrum(spectrum);
-    free_parameters();
- 	}
- 	else{
-   	char* usage = parse_arguments_get_usage("create_ion_files");
-   	result = parse_arguments_get_error(&error_message);
-   	fprintf(stderr, "Error in command line. Error # %d\n", result);
-   	fprintf(stderr, "%s\n", error_message);
-   	fprintf(stderr, "%s", usage);
-   	free(usage);
- 	}
+    exit(1);
+  }
+
+  // prepare the spectrum 
+  carp(CARP_INFO, "Normalizing spectrum %i", scan_num);
+  sum_normalize_spectrum(spectrum);
+
+  carp(CARP_INFO, "Ranking spectrum peaks %i", scan_num);
+  spectrum_rank_peaks(spectrum); 
+
+  // parse the peptides
+  int num_lines;
+  carp(CARP_INFO, "Parsing peptides from %s", peptide_file_name);
+  char** peptides = parse_file(peptide_file_name, MAX_PEPTIDES, &num_lines);
+      
+  carp(CARP_INFO, "Creating and outputting ions");
+
+  // output GMTK peptide ion files
+  // PAIRED add output_psm_files_paired and change this to 
+  // output_psm_files_single
+  BOOLEAN_T return_status = FALSE;
+  if (strcmp(model_type, "single") == 0){
+    return_status = output_psm_files_single(
+        output_directory, spectrum, peptides, num_lines, 
+        charge, starting_sentence_idx);
+  } else if (strcmp(model_type, "paired") == 0){
+    return_status = output_psm_files_paired(
+        output_directory, spectrum, peptides, num_lines, 
+        charge, starting_sentence_idx);
+  }
+
+  if (return_status == FALSE){
+    carp(CARP_FATAL, "Failed to create ion files for: %s %i %s.", 
+       ms2_file, scan_num, peptide_file_name);
+  } else {
+    carp(CARP_INFO, "Done outputting files.");
+  }
+
+  // free heap 
+  int peptide_idx = 0;
+  for (peptide_idx=0; peptide_idx < num_lines; peptide_idx++){
+    free(peptides[peptide_idx]);
+  }
+  free(peptides);
+  free_spectrum_collection(collection);
+  free_spectrum(spectrum);
+  free_parameters();
  	exit(0);
 }
