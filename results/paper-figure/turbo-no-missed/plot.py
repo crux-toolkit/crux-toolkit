fb57d9fcab1312ce9776dcd3e3cbdf8e17767e83
aklammer <aklammer@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2008-02-13 22:56:35 +0000

    *** empty log message ***

--- results/paper-figure/turbo-no-missed/plot.py
+++ results/paper-figure/turbo-no-missed/plot.py
@@ -5,6 +5,7 @@ names = ["crux", "crux.no", "sequest", "sequest.no"]
 styles = ["k--o", "k-o", "m--s", "m-s"]
 labels = ["Crux (w/ index)", "Crux (w/o index)", "Sequest (w/ index)", "Sequest (w/o) index"]
 
+NUM_SPECTRA = 100.0
 idx = 0
 for name in names:
   ys = []
@@ -16,7 +17,7 @@ for name in names:
       if line.startswith("real"):
         secondsTime = float(line.split("m")[1].split("s")[0])
         minutesTime = float(line.split()[1].split("m")[0])
-        ys.append(secondsTime + 60.0 * minutesTime)
+        ys.append( (secondsTime + 60.0 * minutesTime) / NUM_SPECTRA)
         xs.append(float(window))
     fh.close()
 
@@ -26,10 +27,10 @@ for name in names:
 legend(loc="lower right")
 xlabel("Mass window (Da)", size=20)
 xlim(0.05, 5)
-ylim(0.1, 5000.0)
+ylim(0.001, 50.0)
 xticks(size=20)
 yticks(size=20)
-ylabel("Runtime for 100 spectra (s)", size=20)
+ylabel("Runtime per spectrum (s)", size=20)
 savefig("indexing-yeast-windows.eps")
 savefig("indexing-yeast-windows.png")
       
