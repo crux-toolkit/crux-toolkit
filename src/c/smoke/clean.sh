a7c6b2342d672d07de30b8804948dc3ba48b8680
frewen <frewen@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2008-02-20 23:41:05 +0000

    chages to smoke to accomodate new generate peptides code and for new automake setup.  Added an option to test-crux.pl to specify the path in which executables are found.  Use this option as -p ../ in runall b/c executables are now in src/c dir.  removed  ../../../bin/crux- from test-crux.cmds. updated standard output files to reflect changes after confirming that errors were not introduced.  Changes: some masses were +/- 0.01, peptides of same mass sometimes sorted in different order, with --output-trypticity=T the tags are different.  Id like them to go back to the previous way, but its not important.

--- src/c/smoke/clean.sh
+++ src/c/smoke/clean.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -f crux_match* gmon.out target.sqt decoy.sqt get_ms2_spectrum.out test*csm out error
+rm -f crux_match* gmon.out target.sqt decoy.sqt get_ms2_spectrum.out test*csm out error test-perc.sqt
