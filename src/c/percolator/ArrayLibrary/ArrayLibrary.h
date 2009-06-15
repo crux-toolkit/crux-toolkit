720b0d1fa818bc3af7a431719661f6ef536a616f
cegrant <cegrant@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2009-06-15 18:07:21 +0000

    Integrated percolator 1.05 into the crux source tree

---
+++ src/c/percolator/ArrayLibrary/ArrayLibrary.h
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ Copyright (c) 2008 Oliver Serang
+
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software. 
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+ 
+ $Id: ArrayLibrary.h,v 1.3 2008/05/20 00:24:43 lukall Exp $
+ 
+ *******************************************************************************/
+#ifndef _ArrayLibrary_H
+#define _ArrayLibrary_H
+
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+#ifdef DEBUG
+#define SAFE_ARRAYS
+#endif
+
+using namespace std;
+
+// forward declarations
+template <typename T> class Array;
+template <typename T> class OrderedArray;
+template <typename T> class PackedArray;
+
+#include "Array.h"
+#include "OrderedArray.h"
+#include "ArrayTypes.h"
+#include "Numerical.h"
+#include "Arithmetic.h"
+#include "VectorFunctions.h"
+#include "MatrixFunctions.h"
+#include "Random.h"
+#include "PackedVec.h"
+#include "PackedSetFunctions.h"
+#include "PackedMatrix.h"
+
+#include "Array.cpp"
+#include "OrderedArray.cpp"
+#include "Arithmetic.cpp"
+
+#endif
+
