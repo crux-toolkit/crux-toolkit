637a45d668a94783abfc56ce96acd97e3f994d20
cegrant <cegrant@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2009-09-02 20:28:52 +0000

    Added --disable-search option to configure.ac. This disables the spectrum/protein database search function, allowing an open source distribution of crux. Code review by Barbara

--- src/c/search.h
+++ src/c/search.h
@@ -13,35 +13,6 @@
 extern "C" {
 #endif
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <time.h>
-#include "carp.h"
-#include "peptide.h"
-#include "protein.h"
-#include "parse_arguments.h"
-#include "parameter.h"
-#include "spectrum.h"
-#include "spectrum_collection.h"
-#include "generate_peptides_iterator.h"
-#include "crux-utils.h"
-#include "scorer.h"
-#include "objects.h"
-#include "match.h"
-#include "match_collection.h"
-
-#define NUM_SEARCH_OPTIONS 16
-#define NUM_SEARCH_ARGS 2
-
-/* Private functions */
-int prepare_protein_input(char* input_file, 
-                          INDEX_T** index, 
-                          DATABASE_T** database);
-void open_output_files(FILE*** binary_filehandle_array, 
-                       FILE** sqt_filehandle,
-                       FILE** decoy_sqt_filehandle);
-
 int search_main(int argc, char** argv);
 
 #ifdef __cplusplus
