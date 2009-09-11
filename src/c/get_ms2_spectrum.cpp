ca1037f17a44433735549d1b8260e71619052bb5
wnoble <wnoble@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2009-09-11 20:48:56 +0000

    Add to crux-get-ms2-spectrum the ability to retrieve multiple scans.  Code review by Charles Grant.

--- src/c/get_ms2_spectrum.cpp
+++ src/c/get_ms2_spectrum.cpp
@@ -19,18 +19,43 @@
 #include "spectrum_collection.h"
 #include "unistd.h"
 
+/****************************************************************************
+ * Read a string into either a single positive integer or a range of
+ * positive integers, depending on whether it contains a hyphen.
+ ****************************************************************************/
+static void parse_scan_numbers
+  (char* my_string,
+   int*  min_scan,
+   int*  max_scan)
+{
+  // Does the string contain a hyphen?
+  int string_index = 0;
+  while (my_string[string_index] != '\0') {
+    if (my_string[string_index] == '-') {
+      my_string[string_index] = '\0';
+      *min_scan = atoi(my_string);
+      *max_scan = atoi(my_string + string_index + 1);
+      assert((*min_scan > 0) && (*max_scan > 0)); 
+      assert(*min_scan < *max_scan);
+      return;
+    }
+    string_index++;
+  }
+  fprintf(stderr, "No hyphen!\n");
+
+  // If no hyphen, then the range is just a single integer.
+  *min_scan = *max_scan = atoi(my_string);
+}
+
+/****************************************************************************
+ * MAIN
+ ****************************************************************************/
 #define NUM_MS2_OPTIONS 3
-#define NUM_MS2_ARGUMENTS 3
+#define NUM_MS2_ARGUMENTS 2
 
 int main(int argc, char** argv){
 
   /* Declarations */
-  SPECTRUM_COLLECTION_T * collection; ///<spectrum collection
-  SPECTRUM_T * spectrum; ///<the spectrum of interest
-  BOOLEAN_T spectrum_found; ///<did we find the spectrum?
-  int scan_number; ///< the query scan number
-  FILE* output_file; ///< output file name
-  //OUTPUT_WRITE_MODE_T file_flag;
   BOOLEAN_T options = FALSE; ///< do we want options?
 
   /* Define optional command line arguments */
@@ -39,18 +64,15 @@ int main(int argc, char** argv){
     "version", 
     "stats", 
     "verbosity"/*,
-    "use-mstoolkit"*/}; //out-file
+    "use-mstoolkit"*/};
 
   int num_arguments = NUM_MS2_ARGUMENTS;
   const char* argument_list[NUM_MS2_ARGUMENTS] = {
     "scan number",
-    "ms2 file",
-    "output file"
+    "ms2 file"
   };
 
   /* for debugging parameter handling */
-  //set_verbosity_level(CARP_DETAILED_DEBUG);
-  //set_verbosity_level(CARP_DETAILED_DEBUG);
   set_verbosity_level(CARP_ERROR);
 
   /* set up parameters and default values */
@@ -67,83 +89,77 @@ int main(int argc, char** argv){
   //set_verbosity_level(get_int_parameter("verbosity"));
 
   /* Get arguments */
-  scan_number = get_int_parameter("scan number");
+  int min_scan;
+  int max_scan;
+  parse_scan_numbers(get_string_parameter("scan number"), &min_scan, &max_scan);
+  fprintf(stderr, "Scanning from %d to %d.\n", min_scan, max_scan);
+				   
   char* ms2_filename = get_string_parameter_pointer("ms2 file");
-  char* output_filename = get_string_parameter_pointer("output file");
   carp(CARP_DETAILED_DEBUG, "ms2_filename: %s", ms2_filename);
-  carp(CARP_DETAILED_DEBUG, "output_filename: %s", output_filename);
 
   /* Get options */
   options = get_boolean_parameter("stats");
-  //file_flag = get_output_mode_parameter("out-file");
 
   /* read input file */
   if( access(ms2_filename, F_OK)){
     carp(CARP_FATAL, "Could not read from ms2 file '%s'", ms2_filename);
   }
   carp(CARP_DETAILED_DEBUG, "Creating spectrum collection.");
-  collection = new_spectrum_collection(ms2_filename);
-  spectrum = allocate_spectrum();
-  
-  /* search for spectrum with correct scan number */
-  spectrum_found = get_spectrum_collection_spectrum(collection, 
-                                                    scan_number, 
-                                                    spectrum);
-  if( !spectrum_found ){
-    carp(CARP_FATAL, "Could not find scan number %i", scan_number);
-  }
+  SPECTRUM_COLLECTION_T* collection = new_spectrum_collection(ms2_filename);
 
-  carp(CARP_INFO, "Found a spectrum with scan number %i", scan_number);
+  int scan_number;
+  int num_found = 0;
+  for (scan_number = min_scan; scan_number <= max_scan; scan_number++) {
+    SPECTRUM_T* spectrum = allocate_spectrum();
 
-  if( !access(output_filename, F_OK)){//&& file_flag != FILE_REPLACE
-    carp(CARP_FATAL, "The output file '%s' already exists.  " \
-         "Use the --out-file option to overwrite or replace.",
-         output_filename);
-  }
-
-  output_file = fopen(output_filename, "w");//output_mode_to_string(file_flag)
-
-  if( access(output_filename, W_OK)){
-    carp(CARP_FATAL, 
-         "Could not write spectrum to '%s'.  Check file permissions", 
-         output_filename);
-  }
-
-  print_spectrum(spectrum, output_file);
-  fclose(output_file);
-
-  int charge_state_index = 0; 
-  int charge_state_num = get_spectrum_num_possible_z(spectrum);
-  int* possible_z_array = get_spectrum_possible_z(spectrum);
-  int possible_z;
-  
-  /* Print stats if requested */
-  if(options){
-    printf("Scan number: %i\n", scan_number);
-    printf("Precursor m/z:%.2f\n", get_spectrum_precursor_mz(spectrum));
-    printf("Total Ion Current:%.2f\n", get_spectrum_total_energy(spectrum));
-    printf("Base Peak Intensity:%.1f\n", 
-           get_spectrum_max_peak_intensity(spectrum)); // base is max
-    printf("Number of peaks:%d\n", get_spectrum_num_peaks(spectrum));
-    printf("Minimum m/z:%.1f\n", get_spectrum_min_peak_mz(spectrum));
-    printf("Maximum m/z:%.1f\n", get_spectrum_max_peak_mz(spectrum));
-    
-    for(charge_state_index=0; charge_state_index < charge_state_num; 
-                                                   ++charge_state_index){
-      possible_z = possible_z_array[charge_state_index];
-      printf("Charge state:%d\n", possible_z);
-      printf("Neutral mass:%.2f\n", 
-             get_spectrum_neutral_mass(spectrum, possible_z));
-      printf("Charged mass:%.2f\n", get_spectrum_mass(spectrum, possible_z));
-      printf("M+H+ mass:%.2f\n", 
-             get_spectrum_singly_charged_mass(spectrum, possible_z));
+    /* search for spectrum with the correct scan number */
+    BOOLEAN_T spectrum_found = get_spectrum_collection_spectrum(collection, 
+								scan_number, 
+								spectrum);
+    if( !spectrum_found ){
+      carp(CARP_WARNING, "Could not find scan number %i", scan_number);
+      free_spectrum(spectrum);
+      continue;
     }
-  }
 
-  free(possible_z_array);
-  free_spectrum(spectrum);
-  free_spectrum_collection(collection);
+    /* Print either the spectrum or stats. */
+    if (!options){
+      print_spectrum(spectrum, stdout);
+
+    } else {
+
+      int charge_state_index = 0; 
+      int charge_state_num = get_spectrum_num_possible_z(spectrum);
+      int* possible_z_array = get_spectrum_possible_z(spectrum);
+      int possible_z;
   
+      printf("Scan number: %i\n", scan_number);
+      printf("Precursor m/z:%.2f\n", get_spectrum_precursor_mz(spectrum));
+      printf("Total Ion Current:%.2f\n", get_spectrum_total_energy(spectrum));
+      printf("Base Peak Intensity:%.1f\n", 
+	     get_spectrum_max_peak_intensity(spectrum)); // base is max
+      printf("Number of peaks:%d\n", get_spectrum_num_peaks(spectrum));
+      printf("Minimum m/z:%.1f\n", get_spectrum_min_peak_mz(spectrum));
+      printf("Maximum m/z:%.1f\n", get_spectrum_max_peak_mz(spectrum));
+    
+      for(charge_state_index=0; charge_state_index < charge_state_num; 
+	  ++charge_state_index){
+	possible_z = possible_z_array[charge_state_index];
+	printf("Charge state:%d\n", possible_z);
+	printf("Neutral mass:%.2f\n", 
+	       get_spectrum_neutral_mass(spectrum, possible_z));
+	printf("Charged mass:%.2f\n", get_spectrum_mass(spectrum, possible_z));
+	printf("M+H+ mass:%.2f\n", 
+	       get_spectrum_singly_charged_mass(spectrum, possible_z));
+      }
+      free(possible_z_array);
+    }
+    free_spectrum(spectrum);
+    num_found++;
+  }
+  free_spectrum_collection(collection);
+
+  carp(CARP_INFO, "Found %d spectra.\n", num_found);
   carp(CARP_INFO, "crux-get-ms2-spectrum finished.");
   return(0);
 }
