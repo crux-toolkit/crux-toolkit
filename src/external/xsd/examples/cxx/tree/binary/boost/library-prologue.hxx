f192ecdf39b2d77bc055376e637fb0cdc42457d3
cegrant <cegrant@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2012-10-19 01:07:46 +0000

    XSD and Xerces don't need to be included in svn, we pick them up from their repositories during the build

--- src/external/xsd/examples/cxx/tree/binary/boost/library-prologue.hxx
+++
@@ -1,9 +0,0 @@
-// Include declarations for the archive types.
-//
-#include <boost/archive/text_oarchive.hpp>
-#include <boost/archive/text_iarchive.hpp>
-
-// Include insertion/extraction operators for fundamental types.
-//
-#include "boost-archive-insertion.hxx"
-#include "boost-archive-extraction.hxx"
