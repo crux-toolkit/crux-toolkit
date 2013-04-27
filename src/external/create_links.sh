454797d1a24f6e65c25e87ee9791f9f2acf9daba
cegrant <cegrant@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2013-04-27 00:37:06 +0000

    Merged minor changes to the build from crux-percolator branch. These eliminate possible shell and compile errors on some platforms. svn merge -c 14513 file:///noble/svn/crux/branches/crux-percolator svn merge -c 13960 file:///noble/svn/crux/branches/crux-percolator

--- src/external/create_links.sh
+++ src/external/create_links.sh
@@ -6,7 +6,6 @@
 echo "CREATING GENERICALLY NAMED SYMBOLIC LINKS TO LIBRARIES"
 
 install_path=.
-STARING_DIR=$(pwd)
 for arg in $*; do
     echo "arg:$arg"
     if [[ $arg = --prefix=* ]]; then
@@ -24,28 +23,26 @@ cd $install_path/lib
 
 
 
-ln -s libboost_date_time-*-mt-s.a libboost_date_time-mt-s.a
-ln -s libboost_date_time-*-mt-s.a libboost_date_time.a
-ln -s libboost_iostreams-*-mt-s.a libboost_iostreams-mt-s.a 
-ln -s libboost_iostreams-*-mt-s.a libboost_iostreams.a 
-ln -s libboost_serialization-*-mt-s.a libboost_serialization-mt-s.a
-ln -s libboost_serialization-*-mt-s.a libboost_serialization.a
-ln -s libboost_thread-*-mt-s.a libboost_thread-mt-s.a 
-ln -s libboost_thread-*-mt-s.a libboost_thread.a 
-ln -s libgd-*-mt-s-2_1.a libgd-mt-s-2_1.a   
-ln -s libgd-*-mt-s-2_1.a libgd.a   
-ln -s libz-*-mt-s-1_2.a libz-mt-s-1_2.a
-ln -s libz-*-mt-s-1_2.a libz.a
-ln -s libboost_filesystem-*-mt-s.a libboost_filesystem-mt-s.a 
-ln -s libboost_filesystem-*-mt-s.a libboost_filesystem.a 
-ln -s libboost_regex-*-mt-s.a  libboost_regex-mt-s.a     
-ln -s libboost_regex-*-mt-s.a  libboost_regex.a     
-ln -s libboost_system-*-mt-s.a libboost_system-mt-s.a 
-ln -s libboost_system-*-mt-s.a libboost_system.a 
-ln -s libfreetype-*-mt-s-2_4.a  libfreetype-mt-s-2_4.a
-ln -s libfreetype-*-mt-s-2_4.a  libfreetype.a
-ln -s libpng-*-mt-s-1_5.a libpng-mt-s-1_5.a
-ln -s libpng-*-mt-s-1_5.a libpng.a
-
-cd $STARTING_DIR
+ln -s libboost_date_time-*-mt.a libboost_date_time-mt-s.a
+ln -s libboost_date_time-*-mt.a libboost_date_time.a
+ln -s libboost_iostreams-*-mt.a libboost_iostreams-mt-s.a 
+ln -s libboost_iostreams-*-mt.a libboost_iostreams.a 
+ln -s libboost_serialization-*-mt.a libboost_serialization-mt-s.a
+ln -s libboost_serialization-*-mt.a libboost_serialization.a
+ln -s libboost_thread-*-mt.a libboost_thread-mt-s.a 
+ln -s libboost_thread-*-mt.a libboost_thread.a 
+ln -s libgd-*-mt-2_1.a libgd-mt-s-2_1.a   
+ln -s libgd-*-mt-2_1.a libgd.a   
+ln -s libz-*-mt-1_2.a libz-mt-s-1_2.a
+ln -s libz-*-mt-1_2.a libz.a
+ln -s libboost_filesystem-*-mt.a libboost_filesystem-mt-s.a 
+ln -s libboost_filesystem-*-mt.a libboost_filesystem.a 
+ln -s libboost_regex-*-mt.a  libboost_regex-mt-s.a     
+ln -s libboost_regex-*-mt.a  libboost_regex.a     
+ln -s libboost_system-*-mt.a libboost_system-mt-s.a 
+ln -s libboost_system-*-mt.a libboost_system.a 
+ln -s libfreetype-*-mt-2_4.a  libfreetype-mt-s-2_4.a
+ln -s libfreetype-*-mt-2_4.a  libfreetype.a
+ln -s libpng-*-mt-1_5.a libpng-mt-s-1_5.a
+ln -s libpng-*-mt-1_5.a libpng.a
 
