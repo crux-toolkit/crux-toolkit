8bc850333258b54382133489692df3dcf1a3d332
sjoemac <sjoemac@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2013-09-12 20:00:19 +0000

    More files that were not added for comet...

---
+++ doc/user/comet-options/sidebar.php
@@ -0,0 +1,35 @@
+   <div id="sidebar">
+      <div id="updates" class="orangebox">
+         <h2><a href="/updates">Recent Updates</a></h2>
+            <ul>
+               <li><b>2013/02/21</b>: I just posted two known issues to the
+                   <a href="http://groups.google.com/group/comet-ms">Google group</a> as well
+                   as on the <a href="/release/201201/">release page</a>.
+               <li><b>2013/02/21</b>: I created a <a href="http://groups.google.com/group/comet-ms">Comet
+                   Google group</a> for questions, issues, and announcements.
+               <li><b>2013/02/04</b>: <a href="/release/201201">Release 2012.01 rev. 3</a>
+                   Another, hopefully final maintenance release of Comet version 2012.01.</li>
+               <p/>
+            </ul>
+      </div>
+      <div id="updates" class="orangebox">
+         <h2>Links</h2>
+            <ul>
+               <li><a href="http://www.matrixscience.com/cgi/search_form.pl?FORMVER=2&SEARCH=MIS">Mascot</a> search engine</li>
+               <li><a href="http://pubchem.ncbi.nlm.nih.gov/omssa/index.htm">OMSSA</a> search engine</li>
+               <li><a href="http://h.thegpm.org/tandem/thegpm_tandem.html">X!Tandem</a> search engine</li>
+               <li><a href="http://www.ncbi.nlm.nih.gov/CBBresearch/qmbp/raid_dbs/">RAId_DbS</a> search engine</li>
+               <li><a href="http://prospector.ucsf.edu/prospector/mshome.htm">ProteinProspector</a> search tools</li>
+               <li><a href="https://sourceforge.net/projects/sashimi/">Sashimi</a> TPP project</li>
+               <li><a href="http://proteowizard.sourceforge.net/">ProteoWizard</a> library &amp; tools</li>
+               <li><a href="http://open-ms.sourceforge.net/">OpenMS</a> library &amp; tools</li>
+               <li><a href="http://noble.gs.washington.edu/proj/crux/">Crux</a> project</li>
+               <li><a href="http://code.google.com/p/mstoolkit/">MSToolkit</a> data parsing library</li>
+               <li><a href="http://code.google.com/p/lorikeet/">Lorikeet</a> spectrum viewer</li>
+               <li><a href="http://www.sharedproteomics.com/">SharedProteomics</a> forum/site</li>
+               <li><a href="http://proteomicsresource.washington.edu/">UW Proteomics Resource</a></li>
+            </ul>
+      </div>
+
+   </div>
+   <!-- end #sidebar -->
