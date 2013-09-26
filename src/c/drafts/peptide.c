061f841c27ce50abd1764e6c26f212c02462a3f3
kaipot <kaipot@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2013-09-26 21:59:08 +0000

    Removed unused directories in src/c

--- src/c/drafts/peptide.c
+++
@@ -1,9 +0,0 @@
-typedef struct Peptide
-{
-    char PrefixAmino; // The base BEFORE the peptide starts.  (Useful for checking trypsin fragmentation)
-    char SuffixAmino; // The base AFTER the peptide starts.
-    int AminoIndex[MAX_PT_MODS]; 
-    MassDelta* ModType[MAX_PT_MODS];
-    struct PeptideMatch* First;
-} Peptide;
-
