55917a22e920799bbdc326a9c09ff613777606bf
sjoemac <sjoemac@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2013-10-09 21:59:21 +0000

    Updated comet to checkout latest release and update Makefiles to accept make CXXFLAGS=-DCRUX.  Code reviewed by Charles.

--- src/external/patch_comet.sh
+++ src/external/patch_comet.sh
@@ -11,9 +11,5 @@ BIN_PREFIX=$2
 echo "Copying from " $1 "to" $2
 
 cp  $SRC_PREFIX/patches/comet/Makefile  $BIN_PREFIX/build/src/comet/Makefile
-cp  $SRC_PREFIX/patches/comet/CometSearch/CometPreprocess.h $BIN_PREFIX/build/src/comet/CometSearch/CometPreprocess.h
-cp  $SRC_PREFIX/patches/comet/CometSearch/Common.h $BIN_PREFIX/build/src/comet/CometSearch/Common.h
 cp  $SRC_PREFIX/patches/comet/CometSearch/Makefile $BIN_PREFIX/build/src/comet/CometSearch/Makefile
-cp $SRC_PREFIX/patches/comet/MSToolkit/Makefile $BIN_PREFIX/build/src/comet/MSToolkit/Makefile
-cp $SRC_PREFIX/patches/comet/MSToolkit/include/H5public.h  $BIN_PREFIX/build/src/comet/MSToolkit/include/H5public.h
-cp $SRC_PREFIX/patches/comet/MSToolkit/include/mzParser.h $BIN_PREFIX/build/src/comet/MSToolkit/include/mzParser.h
+
