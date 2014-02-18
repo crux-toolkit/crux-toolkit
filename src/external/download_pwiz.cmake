ff887a3782cbf6a3c78134100fdeed5999e59cd1
charlesegrant <charlesegrant@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2014-02-18 18:58:54 +0000

    Updated download_pwiz.cmake to use wget rather than CMake file command, which doesn't support HTTPS

--- src/external/download_pwiz.cmake
+++ src/external/download_pwiz.cmake
@@ -1,83 +1,99 @@
 # This is a CMake script for downloading the partial source distribution of 
-# Proteowizard from its Team City repository. To get the URL for this
-# distribution we have to find the build id, then use the build id to look up
-# the version number. Once we have the version number we can build the URL for 
-# downloading the distribution.
+# Proteowizard from its Team City repository.
+#
+# We have to use 'wget' for downloading because the Proteowizard site uses
+# HTTPS rather than HTTP. HTTPS is not directly supported by the standard 
+# build of CMake.
+#
+# To get the URL for the distribution tarball we have to find the build id, 
+# then use the build id to look up the Proteowizard version number.
+# Once we have the version number we can build the URL for downloading the distribution.
 
-# Team City performs several different builds. We want the one
-# for Pwiz source without Skyline but with vendor library suppport.
-
-# This script expects to variables to be passed on the command line:
+# This script expects two variables to be passed on the command line:
 #   DOWNLOAD_DIR, the location to store the downloaded tar ball.
 #   WORKING_DIR, the location where the expanded source files
 
-set(build_type "bt81")
-
 # This macro checks download status codes for errors
-macro (check_status download_name status)
-  list(GET status 0 status_code)
-  list(GET status 1 status_message)
+macro (check_status download_name status_code error_message)
   if (${status_code} EQUAL 0)
     message(STATUS "Downloaded ${download_name}.")
   else (${status_code} EQUAL 0)
     message(
       FATAL_ERROR 
-      "Unable to download ${download_name}: ${status_message} "
-      "\nError code: ${status_code}." 
+      "Unable to download ${download_name} "
+      "\nError message: ${error_message}." 
       "\nDownload of Proteowizard failed."
     )
   endif (${status_code} EQUAL 0)
 endmacro (check_status)
 
-# Get the build id
-file(
-  DOWNLOAD 
-  "http://teamcity.labkey.org:8080/app/rest/buildTypes/id:${build_type}/builds?status=SUCCESS&count=1&guest=1"
-  ${DOWNLOAD_DIR}/pwiz.build-info.txt
-  STATUS status
+# 'wget' has to be available
+find_program(wget "wget")
+if (${wget} STREQUAL "wget-NOTFOUND")
+  message(
+    FATAL_ERROR
+    "The program 'wget' was not found.\n"
+    "'wget' is required in order to download ProteoWizard."
+  )
+endif (${wget} STREQUAL "wget-NOTFOUND")
+
+# Team City performs several different builds. We want the one
+# for Pwiz source without Skyline but with vendor library suppport.
+set(build_type "bt81")
+
+set(
+  build_info_url 
+  http://teamcity.labkey.org:8080/app/rest/buildTypes/id:${build_type}/builds?status=SUCCESS&count=1&guest=1
 )
-check_status("build ID" status)
+execute_process(
+  COMMAND ${wget} --no-check-certificate -nv -O - "${build_info_url}"
+  RESULT_VARIABLE status
+  OUTPUT_VARIABLE build_info 
+  ERROR_VARIABLE error_message
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+check_status("build info" status ${error_message})
 
 # Using the build id download the version string
-file(STRINGS "${DOWNLOAD_DIR}/pwiz.build-info.txt" BUILD_INFO)
-string(REGEX REPLACE "^.*build id=\"([0-9]+)\".*$" "\\1" BUILD_ID ${BUILD_INFO})
-file(
-  DOWNLOAD 
-  "http://teamcity.labkey.org:8080/repository/download/${build_type}/${BUILD_ID}:id/VERSION?guest=1"
-  ${DOWNLOAD_DIR}/pwiz.version-info.txt
-  STATUS status
+string(REGEX REPLACE "^.*build id=\"([0-9]+)\".*$" "\\1" build_id "${build_info}")
+set(build_info_url "http://teamcity.labkey.org/repository/download/${build_type}/${build_id}:id/VERSION?guest=1")
+execute_process(
+  COMMAND ${wget} --no-check-certificate -nv -O - "${build_info_url}"
+  RESULT_VARIABLE status
+  OUTPUT_VARIABLE version_info 
+  ERROR_VARIABLE error_message
+  OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-check_status("version string" status)
+check_status("version string" status ${error_message})
 
 # Use the version string to build the URL
 set(
-  BASE_DOWNLOAD_URL
+  base_download_url
   "http://teamcity.labkey.org:8080/guestAuth/repository/download/${build_type}/.lastSuccessful/"
 )
-file(STRINGS "${DOWNLOAD_DIR}/pwiz.version-info.txt" VERSION_INFO)
-string(REGEX REPLACE "^([0-9]+)\\.([0-9]+)\\.([0-9]+).*$" "\\1_\\2_\\3" VERSION_ID ${VERSION_INFO})
-set(filename pwiz-src-${VERSION_ID}.tar.bz2)
+string(REGEX REPLACE "^([0-9]+)\\.([0-9]+)\\.([0-9]+).*$" "\\1_\\2_\\3" version_id ${version_info})
+set(filename pwiz-src-${version_id}.tar.bz2)
 set(
-  DOWNLOAD_URL 
-  "${BASE_DOWNLOAD_URL}${filename}"
+  download_url 
+  "${base_download_url}${filename}"
 )
 # Using the version string download the file
-file(
-  DOWNLOAD 
-  ${DOWNLOAD_URL}
-  "${DOWNLOAD_DIR}/${filename}"
-  STATUS status
+execute_process(
+  COMMAND ${wget} --no-check-certificate -nv -O ${DOWNLOAD_DIR}/${filename} "${download_url}"
+  RESULT_VARIABLE status
+  ERROR_VARIABLE error_message
+  OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-check_status("Proteowizard distribution" status)
+check_status("Proteowizard distribution" status ${error_message})
 
 # Expand the tar file into the working directory
 execute_process(
   COMMAND ${CMAKE_COMMAND} -E tar xf ${DOWNLOAD_DIR}/${filename}
+  RESULT_VARIABLE status
   WORKING_DIRECTORY ${WORKING_DIR}
-  RESULT_VARIABLE rv
 )
-if (${rv} EQUAL 0)
+if (${status} EQUAL 0)
   message(STATUS "Generated ProteoWizard source files.")
-else (${rv} EQUAL 0)
+else (${status} EQUAL 0)
   message(FATAL_ERROR "Unable to generate ProteoWizard source files.")
-endif (${rv} EQUAL 0)
+endif (${status} EQUAL 0)
