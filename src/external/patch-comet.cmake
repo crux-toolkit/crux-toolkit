54183b9b8e292fceddb6f400d2bc80e3156cfde4
charlesegrant <charlesegrant@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2014-06-04 00:59:52 +0000

    Added code to patch Comet.sln on Windows

---
+++ src/external/patch-comet.cmake
@@ -0,0 +1,38 @@
+# This is a CMake script for patching Comet on Windows for use in the Crux
+# build.
+
+# This script expects two variables to be passed on the command line:
+#   SOURCE_DIR, the path to the directory containing the patch source.
+#   BINARY_DIR, the path to the directory containing the patch destination.
+
+# This macro checks download status codes for errors
+macro (check_status status_code)
+  if (${status_code} EQUAL 0)
+    message(STATUS "Patched Comet.")
+  else ()
+    message(
+      FATAL_ERROR 
+      "Comet patch failed"
+    )
+  endif (${status_code} EQUAL 0)
+endmacro (check_status)
+
+# This macro checks download status codes for errors
+if (WIN32 AND NOT CYGWIN)
+    message(${SOURCE_DIR}/patches/comet/Comet.rc)
+    message(${BINARY_DIR}/build/src/comet/Comet.rc)
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E copy 
+        ${SOURCE_DIR}/patches/comet/Comet.rc
+        ${BINARY_DIR}/build/src/comet/Comet.rc
+        RESULT_VARIABLE status
+    )
+    check_status(status)
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E copy 
+        ${SOURCE_DIR}/patches/comet/Comet.sln
+        ${BINARY_DIR}/build/src/comet/Comet.sln
+        RESULT_VARIABLE status
+    )
+    check_status(status)
+endif (WIN32 AND NOT CYGWIN)
