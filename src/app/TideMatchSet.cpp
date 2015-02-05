85306c2706a89156812001d9355f8a2306ad3a9f
kfattila <kfattila@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2015-02-05 19:57:51 +0000

    sorting bug in assign-conf and peptide-centirc output bug fixes.

--- src/app/TideMatchSet.cpp
+++ src/app/TideMatchSet.cpp
@@ -641,6 +641,7 @@ void TideMatchSet::writeHeaders(
     if (header == DISTINCT_MATCHES_SPECTRUM_COL){
       if (peptide_centric == true ) {
         *file << get_column_header(DISTINCT_MATCHES_PEPTIDE_COL);
+        *file << get_column_header(DISTINCT_MATCHES_SPECTRUM_COL);
       } else {
         *file << get_column_header(DISTINCT_MATCHES_SPECTRUM_COL);
       }
