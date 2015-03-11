74192e9cd1908d07e570e1f41728831b0417427b
wsnoble <wsnoble@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2015-03-11 23:53:36 +0000

    Add (as-yet-unfinished) python version of performance test script.

---
+++ test/performance-tests/run-performance-test.py
@@ -0,0 +1,192 @@
+#!/bin/env python
+# CREATE DATE: 11 March 2015
+# AUTHOR: William Stafford Noble
+# Derived from run-performance-test.sh.
+
+# This script runs a performance test on the crux toolkit.  Its usage
+# and output are described in ./performance.html.  The MS2 file comes
+# from the PAnDA paper by Hoopman et al. (JPR 2009).  Extensive
+# documentation of this particular data set is available from
+# crux-projects/panda-data.
+import sys
+import subprocess
+import os
+
+# The location of the crux binary.
+CRUX = "../../src/crux"
+
+# Input files.
+database = "worm+contaminants"
+ms2 = "051708-worm-ASMS-10.ms2"
+
+#############################################################################
+# Run a command with error checking.
+def runCommand(command, outputFileName):
+
+  # Skip the command if the output file already exists.
+  if (outputFileName != "") and (os.path.exists(outputFileName)):
+    sys.stderr.write("%s exists.\n" % outputFileName)
+    return
+  else:
+    sys.stderr.write("%s does not exist.\n" % outputFileName)
+  
+  sys.stderr.write("RUN: %s\n" % command)
+  try:
+    returnCode = subprocess.call(command, shell=True)
+    if (returnCode != 0):
+      sys.stderr.write("Child was terminated by signal %d\n" % -returnCode)
+      sys.exit(1)
+  except OSError, e:
+    sys.stderr.write("Execution failed: %s\n" % e)
+    sys.exit(1)
+
+#############################################################################
+def createParameterFile(parameterFileName):
+  parameterFile = open(parameterFileName, "w")
+
+  # Enzymatic digestion rules.
+  parameterFile.write("enzyme=trypsin\n")
+  parameterFile.write("search_enzyme_number=1\n") 
+  parameterFile.write("digestion=full-digest\n")
+  parameterFile.write("num_enzyme_termini=2\n")
+  parameterFile.write("missed-cleavages=0\n")
+  parameterFile.write("allowed_missed_cleavage=0\n")
+  
+  # Minimums
+  parameterFile.write("minimum_peaks=10\n")
+  parameterFile.write("min-peaks=10\n")
+  
+  # Precursor selection rules.
+  parameterFile.write("precursor-window=3\n")
+  parameterFile.write("precursor-window-type=mass\n")
+  parameterFile.write("peptide_mass_tolerance=3\n")
+  parameterFile.write("peptide_mass_units=0\n") # 0=amu, 1=mmu, 2=ppm
+  
+  # Precursor mass type.
+  parameterFile.write("isotopic-mass=mono\n")
+  parameterFile.write("monoisotopic-precursor=T\n")
+  parameterFile.write("mass_type_parent=1\n") # 1=monoisotopic
+  
+  # Fragment mass type.  Tides uses only monoisotopic.
+  parameterFile.write("fragment-mass=mono\n")
+  parameterFile.write("mass_type_fragment=1\n") # 1=monoisotopic
+  
+  # Decoys.
+  parameterFile.write("decoy-format=peptide-reverse\n")
+  parameterFile.write("num-decoys-per-target=1\n")
+  parameterFile.write("keep-terminal-aminos=C\n")
+  parameterFile.write("decoy_search=1\n")  # 1 = concatenated decoy search
+  
+  # Report the top 5 matches.
+  parameterFile.write("num_results=6\n")
+  parameterFile.write("num_output_lines=5\n")
+  parameterFile.write("top-match=5\n")
+  
+  # Precursor removal.
+  parameterFile.write("remove_precursor_peak=1\n")
+  parameterFile.write("remove_precursor_tolerance=15\n")
+  parameterFile.write("remove-precursor-peak=T\n")
+  parameterFile.write("remove-precursor-tolerance=15\n")
+  
+  # Flanking peaks.
+  parameterFile.write("use-flanking-peaks=F\n")
+  parameterFile.write("theoretical_fragment_ions=1\n") # 0 = flanks; 1 = no flanks
+  parameterFile.write("use-neutral-loss-peaks=F\n") 
+
+  # Fragment m/z discretization.
+  parameterFile.write("fragment_bin_offset=0.68\n")
+  parameterFile.write("fragment_bin_tol=1.0005079\n")
+  parameterFile.write("mz-bin-offset=0.68\n")
+  parameterFile.write("mz-bin-width=1.0005079\n")
+  
+  # Other Crux parameters.
+  parameterFile.write("concat=T\n")
+  parameterFile.write("compute-sp=T\n")
+  parameterFile.write("verbosity=40\n")
+  parameterFile.write("overwrite=T\n")
+  parameterFile.write("peptide-list=T\n")
+  
+  # Comet parameters
+  parameterFile.write("output_pepxml=0\n")
+  parameterFile.write("add_C_cysteine=57.021464\n")
+  parameterFile.write("num_threads=1\n") # Multithreaded sometimes dumps core.
+  parameterFile.write("digest_mass_range=200 7200\n")
+  parameterFile.write("max_fragment_charge=2\n")
+  parameterFile.write("isotope_error=0\n")
+  parameterFile.write("use_A_ions=0\n")
+  parameterFile.write("use_B_ions=1\n")
+  parameterFile.write("use_C_ions=0\n")
+  parameterFile.write("use_X_ions=0\n")
+  parameterFile.write("use_Y_ions=1\n")
+  parameterFile.write("use_Z_ions=0\n")
+  parameterFile.write("use_NL_ions=0\n")
+  parameterFile.write("variable_mod01=0.0 X 0 3\n")
+  parameterFile.write("variable_mod02=0.0 X 0 3\n")
+  parameterFile.write("[COMET_ENZYME_INFO]\n")
+  parameterFile.write("0.  No_enzyme              0      -           -\n")
+  parameterFile.write("1.  Trypsin                1      KR          P\n")
+  parameterFile.close()
+
+#############################################################################
+def extractData(inputFileName, columnName, outputFileName):
+  runCommand("%s extract-columns %s \"%s\" > %s" % 
+             (CRUX, inputFileName, columnName, outputFileName), "")
+
+#############################################################################
+def runSearch(outputDirectory, searchName, searchParam, database, 
+              psmFile, confidenceParam):
+
+  runCommand("%s %s --output-dir %s --parameter-file %s %s %s %s"
+             % (CRUX, searchName, outputDirectory, parameterFileName, 
+                searchParam, ms2, database),
+             psmFile)
+
+  confidenceFile = "%s/assign-confidence.target.txt" % outputDirectory
+  runCommand("%s assign-confidence --output-dir %s %s %s" % 
+             (CRUX, outputDirectory, confidenceParam, psmFile), confidenceFile)
+
+  qFile = "%s/%s.q.txt" % (outputDirectory, searchName)
+  extractData(confidenceFile, "tdc q-value", qFile)
+
+
+#############################################################################
+# MAIN
+#############################################################################
+
+# Create the parameter file.
+parameterFileName = "crux.param"
+createParameterFile(parameterFileName)
+
+# Create the index.
+runCommand("%s tide-index --output-dir %s --parameter-file %s %s.fa %s"
+           % (CRUX, database, parameterFileName, database, database), 
+           "%s/tide-index.peptides.target.txt" % database)
+
+# Run three searches (Comet, Tide XCorr, and Tide p-value).
+runSearch("tide-xcorr", "tide-search", "--exact-p-value T", database, 
+          "tide-xcorr/tide-search.txt", "")
+runSearch("tide-p-value", "tide-search", "", database,
+          "tide-p-value/tide-search.txt",
+          "--smaller-is-better T --score \"exact p-value\"")
+runSearch("comet", "comet", "", "%s.fa" % database, 
+          "comet/comet.target.txt", "--score \"e-value\"")
+
+# Make the performance plot.
+gnuplotFileName = "performance.gnuplot"
+gnuplotFile = open(gnuplotFileName, "w")
+gnuplotFile.write("set output \"/dev/null\"\n")
+gnuplotFile.write("set terminal png\n")
+gnuplotFile.write("set xlabel \"q-value threshold\"\n")
+gnuplotFile.write("set ylabel \"Number of accepted PSMs\"\n")
+gnuplotFile.write("set xrange [0:0.1]\n")
+gnuplotFile.write("set key center right\n")
+gnuplotFile.write("plot \"comet/comet.q.txt\" using 1:0 title \"Comet E-value\" with lines\n")
+gnuplotFile.write("replot \"tide-p-value/tide-search.q.txt\" using 1:0 title \"Tide p-value\" with lines\n")
+gnuplotFile.write("replot \"tide-xcorr/tide-search.q.txt\" using 1:0 title \"Tide XCorr\" with lines\n")
+gnuplotFile.write("set output\n")
+gnuplotFile.write("replot\n")
+gnuplotFile.close() 
+runCommand("gnuplot %s > performance.png" % gnuplotFileName, "")
+
+
+# Make the XCorr scatter plots.
