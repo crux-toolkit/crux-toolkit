d9062e70466b607f5bae4918ac88a7a4833dfc46
kaipot <kaipot@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2015-07-07 17:13:48 +0000

    comet patch (bugfix for writing static mods in sqt) Code reviewed by Charles

--- ext/patch-comet.cmake
+++ ext/patch-comet.cmake
@@ -93,6 +93,14 @@ if (WIN32 AND NOT CYGWIN)
         ${SOURCE_DIR}/patches/comet/CometSearch/CometSearchManager.cpp
         ${BINARY_DIR}/build/src/comet/CometSearch/CometSearchManager.cpp
         RESULT_VARIABLE status
+    check_status(status)
+    message(${SOURCE_DIR}/patches/comet/CometSearch/CometWriteSqt.cpp)
+    message(${BINARY_DIR}/build/src/comet/CometSearch/CometWriteSqt.cpp)
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E copy 
+        ${SOURCE_DIR}/patches/comet/CometSearch/CometWriteSqt.cpp
+        ${BINARY_DIR}/build/src/comet/CometSearch/CometWriteSqt.cpp
+        RESULT_VARIABLE status
     )
     check_status(status)
 endif (WIN32 AND NOT CYGWIN)
