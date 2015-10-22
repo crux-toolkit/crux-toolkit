2f2facadb8e890cff953ab11fdffafe997bc53c2
sjoemac <sjoemac@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2015-10-22 21:10:04 +0000

    Added version reporting for percolator, comet, and boost.  Code reviewed by Bill.

--- src/app/PrintVersion.cpp
+++ src/app/PrintVersion.cpp
@@ -5,7 +5,9 @@
 #include "PrintVersion.h"
 #include "crux_version.h"
 #include "pwiz/Version.hpp"
-
+#include "Caller.h"
+#include "CometSearch/Common.h"
+#include "boost/version.hpp"
 using namespace std;
 
 /**
@@ -28,10 +30,23 @@ int PrintVersion::main(int argc, char** argv) {
   (void)argc;
   (void)argv;
 
+  printf("====================\n");
   printf("Crux version %s\n", CRUX_VERSION);
-  
+
+  printf("====================\n");  
   string proteowizard_version = pwiz::Version::str();
   printf("Proteowizard version %s\n", proteowizard_version.c_str());
+  
+  printf("====================\n");
+  string percolator_version = Caller::greeter();
+  printf("%s", percolator_version.c_str());
+  
+  printf("====================\n");
+  printf("Comet version %s\n", comet_version);
+
+  printf("====================\n");
+  printf("Boost version %s\n", BOOST_LIB_VERSION);
+  printf("====================\n");
 
   return 0;
 }
