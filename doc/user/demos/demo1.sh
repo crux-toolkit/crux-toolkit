8c43b2cc578c4e05a17465276f6e51163b9bb4bc
wsnoble <wsnoble@212732f7-e82a-48c5-b261-38c1ba6edc5e>
2016-02-05 19:53:31 +0000

    Minor updates to demo1.sh script.

--- doc/user/demos/demo1.sh
+++ doc/user/demos/demo1.sh
@@ -4,14 +4,19 @@
 
 # This file implements the commands described in ../search-tutorial.html.
 
-CRUX=../../../src/c/crux
+if [[ -e ../../../src/crux ]]; then
+  CRUX=../../../src/crux
+else
+  CRUX=../../../release/src/crux
+fi    
+    
 
-rm -r yeast-index crux-output
+rm -rf yeast-index crux-output
 cp ../data/demo.ms2 .
 cp ../data/small-yeast.fasta .
 $CRUX tide-index small-yeast.fasta yeast-index
 $CRUX tide-search --compute-sp T demo.ms2 yeast-index
 $CRUX percolator crux-output/tide-search.target.txt 
 
-rdb2html -noformatline crux-output/percolator.target.psms.txt \
-  > crux-output/percolator.target.psms.html
+tsv2html.py --table-only  crux-output/percolator.target.psms.txt \
+     > crux-output/percolator.target.psms.html
