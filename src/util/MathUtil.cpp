57ef2856a715f96ebfcdef464cddf604b896477c
CHARLES E. GRANT <cegrant@glutamine.gs.washington.edu>
2016-11-05 21:52:35 -0700

    Fixes for OSX build

--- src/util/MathUtil.cpp
+++ src/util/MathUtil.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 double MathUtil::Round(double x, int decimals) {
-  double shift = pow(10, decimals);
+  double shift = pow(10.0, decimals);
   return (x >= 0)
     ? floor(x * shift + 0.5) / shift
     : ceil(x * shift - 0.5) / shift;
 }
 
 bool MathUtil::AlmostEqual(double x, double y, int precision) {
-  return abs(x - y) < 5*pow(10, -(precision + 1));
+  return abs(x - y) < 5*pow(10.0, -(precision + 1));
 }
 
 MathUtil::MathUtil() {}
