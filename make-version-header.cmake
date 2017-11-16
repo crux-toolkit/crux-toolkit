527adb958a27c45380fa78ea2108edd9627afec3
CharlesEGrant <cegrant@uw.edu>
2017-11-16 11:35:55 -0800

    Switch to using git show-ref rather than looking in files for commit hash

--- make-version-header.cmake
+++ make-version-header.cmake
@@ -1,22 +1,24 @@
 set(CRUX_VERSION_FULL "${CRUX_VERSION}")
 
 if (EXISTS "${SOURCE_DIR}/.git/HEAD")
-  file(READ "${SOURCE_DIR}/.git/HEAD" PROJECT_SOURCE_VERSION)
-  if ("${PROJECT_SOURCE_VERSION}" MATCHES "^ref: ")
-    string(REGEX REPLACE "^ref: (.*)[\r\n]" "\\1" PROJECT_GIT_REF "${PROJECT_SOURCE_VERSION}")
-    if (EXISTS "${SOURCE_DIR}/.git/${PROJECT_GIT_REF}")
-      file(READ "${SOURCE_DIR}/.git/${PROJECT_GIT_REF}" PROJECT_SOURCE_VERSION)
-      string(STRIP "${PROJECT_SOURCE_VERSION}" PROJECT_SOURCE_VERSION)
-      set(CRUX_VERSION_FULL "${CRUX_VERSION_FULL}-${PROJECT_SOURCE_VERSION}")
-    else (EXISTS "${SOURCE_DIR}/.git/${PROJECT_GIT_REF}")
-      MESSAGE("Version header error: ${SOURCE_DIR}/.git/${PROJECT_GIT_REF} not found")
-    endif (EXISTS "${SOURCE_DIR}/.git/${PROJECT_GIT_REF}")
-  else ("${PROJECT_SOURCE_VERSION}" MATCHES "^ref: ")
-    MESSAGE("Version header error: ${SOURCE_DIR}/.git/HEAD does not match pattern")
-  endif ("${PROJECT_SOURCE_VERSION}" MATCHES "^ref: ")
-else (EXISTS "${SOURCE_DIR}/.git/HEAD")
-  MESSAGE("Version header error: ${SOURCE_DIR}/.git/HEAD not found")
+  execute_process(
+    COMMAND "git" "show-ref" "refs/heads/master"
+    WORKING_DIRECTORY ${SOURCE_DIR}
+    RESULT_VARIABLE GIT_RESULT
+    OUTPUT_VARIABLE PROJECT_SOURCE_VERSION
+  )
+  if (GIT_RESULT EQUAL 0)
+    string(
+      REGEX MATCH "^.[^ ]*"
+      PROJECT_SOURCE_VERSION
+      ${PROJECT_SOURCE_VERSION}
+    )
+    set(CRUX_VERSION_FULL "${CRUX_VERSION_FULL}-${PROJECT_SOURCE_VERSION}")
+  else (GIT_RESULT EQUAL 0)
+      MESSAGE("Git command git show-ref failed: ${PROJECT_SOURCE_VERSION}")
+  endif (GIT_RESULT EQUAL 0)
 endif (EXISTS "${SOURCE_DIR}/.git/HEAD")
+
 
 file(WRITE crux_version.h.txt "#define CRUX_VERSION \"${CRUX_VERSION_FULL}\"\n")
 
