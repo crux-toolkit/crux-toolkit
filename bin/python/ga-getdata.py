44786f16e7b5f8ebe11db50c663754694bbbac13
rita-gwen <rita_gwen@yahoo.com>
2018-12-05 18:01:36 -0800

    Modified the python script to accept key file path as an argument.

--- bin/python/ga-getdata.py
+++ bin/python/ga-getdata.py
@@ -3,18 +3,18 @@ from apiclient.discovery import build
 from datetime import datetime
 from datetime import timedelta
 import os
+import sys
+
+key_file = sys.argv[1]
+if not os.path.isfile(key_file):
+	print("Cannot find a key file at '" + key_file + "'")
+	print("Please specify the path to key file as the first command line argument")
+	sys.exit(1)
 
 # The scope for the OAuth2 request.
 SCOPE = 'https://www.googleapis.com/auth/analytics.readonly'
 
-# The location of the key file with the service account credentials.
-KEY_FILENAME = 'noble-lab-1-da1f4a32c6e8.json'
-
 VIEW_ID = '51277087'
-
-# we expect to find the key file together with this script (in the same folder)
-script_path = os.path.abspath(__file__)
-key_file = os.path.join( os.path.split(script_path)[0], KEY_FILENAME)
 
 # Create service account credentials instance to authorize the GA API query
 credentials = ServiceAccountCredentials.from_json_keyfile_name(key_file, SCOPE)
