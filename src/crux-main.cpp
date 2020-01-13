f0bba3e00b01db2a81bf691a50c7ec529a4178f8
Will Fondrie <fondriew@gmail.com>
2020-01-13 12:48:36 -0800

    Initial start to integration. Note that the builds fail.

--- src/crux-main.cpp
+++ src/crux-main.cpp
@@ -47,6 +47,7 @@
 #include "app/CascadeSearchApplication.h"
 #include "app/AssignConfidenceApplication.h"
 #include "app/SubtractIndexApplication.h"
+#include "app/KojakApplication.h"
 /**
  * The starting point for crux.  Prints a general usage statement when
  * given no arguments.  Runs one of the crux commands, including
@@ -79,6 +80,7 @@ int main(int argc, char** argv) {
     applications.add(new PipelineApplication());
     applications.add(new CascadeSearchApplication());
     applications.add(new AssignConfidenceApplication());
+    applications.add(new KojakApplication());
 
     // Utilities
     applications.addMessage(applications.getListName() +
