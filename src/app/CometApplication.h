7ff0257be9f684a0c1b322096d09972cc0260ccd
Charles E. Grant <cegrant@uw.edu>
2020-08-27 11:29:11 -0700

    Added support for new search_enzyme2_number feature

--- src/app/CometApplication.h
+++ src/app/CometApplication.h
@@ -28,6 +28,7 @@ class CometApplication: public CruxApplication {
   void setVarMod(const std::string& param);
   void setEnzyme(const std::string& param,
                  const std::string& searchParam,
+                 const std::string& search2Param,
                  const std::string& sampleParam,
                  const std::string& missedCleavageParam);
 
