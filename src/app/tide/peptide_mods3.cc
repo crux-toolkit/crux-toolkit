crux-toolkit/crux-toolkit

commit e1ebe0d9ad0dc2d0ee616bcbd3f40fabddd63955

add proces ID to temp files produced by Tide

src/app/tide/peptide_mods3.cc
@@ -42,7 +42,7 @@ DEFINE_int32(modsoutputter_file_threshold, 1000,
 
 static string GetTempName(const string& tempDir, int filenum) {
   char buf[64];
-  sprintf(buf, "modified_peptides_partial_%d", filenum);
+  sprintf(buf, "modified_peptides_partial_%d_%d", getpid(), filenum);
   if (!tempDir.empty()) {
     return FileUtils::Join(tempDir, buf);
   }

doc/user/release-notes.html
@@ -86,6 +86,9 @@
 
 <ul>
   <li>
+    Feb 9, 2021: Added process ID number to the names of temporary
+    files created by Tide, to avoid conflicts.</li>
+  <li>
     December 7, 2020: Updated documentation to remove "+" from the usage statement.
   </li>
   <li>

src/io/MatchCollectionParser.cpp
@@ -57,7 +57,7 @@ void MatchCollectionParser::loadDatabase(
         " supported.\nPlease use the original fasta file instead.");
     } else {
       database = new Database(fasta_file, false);// not memmapped
-      database->transformTextToMemmap(".", true);// is temp
+      database->transformTextToMemmap(".");
       decoy_database = new Database();
     }
     database->parse();

src/model/Database.cpp
@@ -52,7 +52,6 @@ void Database::init(){
   proteins_ = new vector<Protein*>();
   protein_map_ = new map<const char*, Protein*, cmp_str>();
   decoys_ = NO_DECOYS;
-  binary_is_temp_ = false;
 }
 
 /**
@@ -145,7 +144,7 @@ Database::~Database() {
     }
   }
 
-  if( binary_is_temp_ && !binary_filename_.empty() ){
+  if( !binary_filename_.empty() ){
     carp(CARP_DEBUG, "Deleting temp binary fasta %s.", 
          binary_filename_.c_str());
     remove(binary_filename_.c_str());
@@ -508,11 +507,10 @@ bool Database::parse()
  * \returns true if all processes succeed, else false.
  */
 bool Database::transformTextToMemmap(
-  const char* output_dir,
-  bool binary_is_temp
+  const char* output_dir
   ){
 
-  createBinaryFasta(output_dir, binary_is_temp);
+  createBinaryFasta(output_dir);
 
   // set is_memmap to true
   setMemmap(true);
@@ -526,14 +524,13 @@ bool Database::transformTextToMemmap(
 /**
  * Using the fasta file the Database was instantiated with, write a
  * binary protein file in the given directory to use for memory
- * mapping.  If is_temp, delete the file on destruction.  Warns if
+ * mapping.  Delete the file on destruction.  Warns if
  * Database was not opened with a text file.  If the database is to
  * contain decoy proteins, randomizes each protein before
  * serializing.  Also prints a new fasta file of the decoy proteins in
  * the same directory as the binary file.
  */
-void Database::createBinaryFasta(const char* directory, bool is_temp){
-  binary_is_temp_ = is_temp;
+void Database::createBinaryFasta(const char* directory){
 
   if( fasta_filename_.empty() ){
     carp(CARP_WARNING, "No fasta file to transform to binary.");
@@ -763,8 +760,6 @@ Protein* Database::getProteinAtIdx(
   unsigned int protein_idx ///< The index of the protein to retrieve -in
   )
 {
-  //carp(CARP_DETAILED_DEBUG, "Getting db protein idx = %i, num proteins %i", 
-  //     protein_idx, database->proteins.size());
   if( protein_idx >= proteins_->size()){
     carp(CARP_FATAL, 
          "Protein index %i out of bounds.  %i proteins in the database",

src/model/Database.h
@@ -49,7 +49,6 @@ class Database {
   unsigned int pointer_count_; ///< number of pointers referencing this database. 
   long file_size_; ///< the size of the binary fasta file, when memory mapping
   DECOY_TYPE_T decoys_; ///< the type of decoys, none if target db
-  bool binary_is_temp_; ///< should we delete the binary fasta in destructor
 
   /**
    * Parses a database from the text based fasta file in the filename
@@ -148,8 +147,6 @@ class Database {
    * \brief Changes a database from one that reads from a fasta file to
    * one that reads from a binary/memmory mapped protein file.
    *
-   * If database already has binary source (i.e. is_memmap == TRUE), 
-   * returns TRUE.  
    * Opens the fasta file pointed to by filename for reading.  Creates a
    * file with the name given.  Reads in each protein from the text file
    * and serializes it to the output file.  Closes both files.  Changes
@@ -158,8 +155,7 @@ class Database {
    * \returns TRUE if all processes succeed, else FALSE.
    */
   bool transformTextToMemmap(
-    const char* binary_protein_filename,
-    bool binary_is_temp
+    const char* binary_protein_filename
     );
 
   /**
@@ -176,10 +172,10 @@ class Database {
   /**
    * Using the fasta file the Database was instantiated with, write a
    * binary protein file in the given directory to use for memory
-   * mapping.  If is_temp, delete the file on destruction.  Warns if
+   * mapping.  Delete the file on destruction.  Warns if
    * Database was not opened with a text file.
    */
-  void createBinaryFasta(const char* directory, bool is_temp = false);
+  void createBinaryFasta(const char* directory);
 
   /** 
    * Access routines of the form get_<object>_<field> and set_<object>_<field>. 

commit e979d1bab4507cff6d3537c34f3c5be5f826c0e3

Commit e1ebe0d broke the windows build due to a missing include statement specific to windows

src/app/tide/peptide_mods3.cc
@@ -5,6 +5,7 @@
 #ifndef _MSC_VER
 #include <unistd.h>
 #else
+#include <process.h>
 #include <windows.h>
 #endif
 #include <string>