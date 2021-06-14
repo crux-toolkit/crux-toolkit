7d534a68044c029c5e7bac9e95ff42d9eb51c57d
ylu465 <ylu465@uw.edu>
2021-06-13 17:54:01 -0700

    debug coelute

--- src/util/StringUtils.h
+++ src/util/StringUtils.h
@@ -72,13 +72,22 @@ class StringUtils {
   static std::string JoinDoubleVec(std::vector<double>& values, const char delimiter ='\0') {
 	    std::stringstream ss;
 	    for (std::vector<double>::iterator i = values.begin(); i != values.end(); i++) {
-	      if (i != values.begin() && delimiter != '\0') {
-	        ss << delimiter;
-	      }
+	      if (i != values.begin() && delimiter != '\0') { ss << delimiter; }
 	      ss << std::setprecision(10) << *i;
 	    }
 	    return ss.str();
   }
+
+  static std::string JoinDoubleArr(double* values, int size, const char delimiter ='\0') {
+	  if (values == NULL || size <= 0) { return std::string();; }
+
+  	    std::stringstream ss;
+  	    for (int i=0; i<size; ++i) {
+  	    	if (i != 0 && delimiter != '\0') { ss << delimiter; }
+  	    	ss << std::setprecision(10) << values[i];
+  	    }
+  	    return ss.str();
+    }
 
 
   // Split a string on a delimiter
