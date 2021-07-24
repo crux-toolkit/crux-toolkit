6a9972db916f5d465f773ca572a50b7c101eeb32
ylu465 <ylu465@uw.edu>
2021-07-24 14:21:17 -0700

    DIAmeter remove internal sort command to incorporate windows

--- src/io/DIAmeterPSMFilter.cpp
+++ src/io/DIAmeterPSMFilter.cpp
@@ -19,74 +19,7 @@ bool DIAmeterPSMFilter::psm_sorter(const PSMByScanCharge & psm1, const PSMByScan
 	return psm1.xcorr_score_ > psm2.xcorr_score_;
 }
 
-void DIAmeterPSMFilter::loadAndFilter(const char* output_file_name, bool filter) {
-	 fileReader_->reset();
-	 std::vector<PSMByScanCharge> pooled_data_vec;
-
-	 ofstream* output_file = create_stream_in_path(output_file_name, NULL, Params::GetBool("overwrite"));
-	 *output_file << StringUtils::Join(fileReader_->getColumnNames(), '\t').c_str() << endl;
-
-	 int prev_scan = -1, prev_charge = -1;
-	 while (fileReader_->hasNext()) {
-		 int scan = fileReader_->getInteger(scan_idx_);
-		 int charge = fileReader_->getInteger(charge_idx_);
-		 std::vector<std::string> data = fileReader_->getCurrentRowData();
-
-		 double xcorr = fileReader_->getDouble(xcorr_idx_);
-
-		 double ensemble = 0.0;
-		 for (int idx = 0; idx < toagg_column_indices_.size(); idx++) {
-			 int curr_column_idx = toagg_column_indices_.at(idx);
-			 double column_val = fileReader_->getDouble(curr_column_idx);
-			 double curr_column_coeff = toagg_column_coeffs_.at(idx);
-			 ensemble += column_val * curr_column_coeff;
-		 }
-		 // carp(CARP_DEBUG, "scan:%d \t charge:%d \t xcorr:%f \t ensemble:%f \t data_str:%s", scan, charge, xcorr, ensemble, StringUtils::Join(data, '\t').c_str());
-
-
-		 double old_ensemble = StringUtils::FromString<double>(data.at(agg_idx_));
-		 data[agg_idx_] = StringUtils::ToString<double>(ensemble, 6);
-		 // carp(CARP_DEBUG, "old_ensemble:%f \t new_ensemble:%s", old_ensemble, data[agg_idx_].c_str());
-
-		 // deal with the PSMs corresponding to the same (scan, charge) tuple
-		 if (prev_scan != scan || prev_charge != charge) {
-			 if (scan < prev_scan) { carp(CARP_FATAL, "Error! The scan must be sorted ascendingly!"); }
-
-			 if (pooled_data_vec.size() > 0) { processPSMGroup(&pooled_data_vec, output_file, filter); }
-			 pooled_data_vec.clear();
-		 }
-
-		 pooled_data_vec.push_back(PSMByScanCharge(xcorr, ensemble, data));
-		 prev_scan = scan;
-		 prev_charge = charge;
-
-		 fileReader_->next();
-	 }
-	 if (pooled_data_vec.size() > 0) { processPSMGroup(&pooled_data_vec, output_file, filter); }
-
-	 if (output_file) { output_file->close(); delete output_file; }
-}
-
-void DIAmeterPSMFilter::processPSMGroup(std::vector<PSMByScanCharge>* pooled_data_vec, ofstream* output_file, bool filter) {
-	// carp(CARP_DEBUG, "pooled_data_vec:%d", pooled_data_vec->size());
-	sort(pooled_data_vec->begin(), pooled_data_vec->end(), psm_sorter);
-
-	PSMByScanCharge psm_baseline = pooled_data_vec->at(0);
-	double ensemble_baseline = psm_baseline.ensemble_score_ + 0.000001;
-	*output_file << StringUtils::Join(psm_baseline.data_, '\t').c_str() << endl;
-
-	for (int idx = 1; idx < pooled_data_vec->size(); idx++) {
-		PSMByScanCharge psm = pooled_data_vec->at(idx);
-		double ensemble = psm.ensemble_score_;
-		if (filter && (ensemble >= ensemble_baseline)) {
-			*output_file << StringUtils::Join(psm.data_, '\t').c_str() << endl;
-			// carp(CARP_DEBUG, "idx:%d \t xcorr:%f \t ensemble:%f", idx, psm.xcorr_score_, psm.ensemble_score_);
-		}
-	}
-
-}
-
-
+int DIAmeterPSMFilter::getKey(int scan, int charge) { return 10*scan+charge; }
 
 void DIAmeterPSMFilter::parseHeader() {
   for (int idx = 0; idx < NUMBER_MATCH_COLUMNS; idx++) {
@@ -104,17 +37,8 @@ void DIAmeterPSMFilter::parseHeader() {
   double coeff_elution = Params::GetDouble("coeff-elution");
   carp(CARP_DETAILED_DEBUG, "coeff_precursor:%f \t coeff_fragment:%f \t coeff_rtdiff:%f \t coeff_elution:%f", coeff_precursor, coeff_fragment, coeff_rtdiff, coeff_elution );
 
-  MATCH_COLUMNS_T toagg_columns_[] = {
-		  TAILOR_COL, RT_DIFF_COL,
-		  PRECURSOR_INTENSITY_RANK_M0_COL, PRECURSOR_INTENSITY_RANK_M1_COL, PRECURSOR_INTENSITY_RANK_M2_COL,
-		  DYN_FRAGMENT_PVALUE_COL, STA_FRAGMENT_PVALUE_COL,
-		  COELUTE_MS1_COL, COELUTE_MS1_MS2_COL  };
-
-  double toagg_coeffs_[] = {
-		  1.0, -coeff_rtdiff,
-		  -coeff_precursor/3.0, -coeff_precursor/3.0, -coeff_precursor/3.0,
-		  coeff_fragment/2.0, coeff_fragment/2.0,
-		  coeff_elution/2.0, coeff_elution/2.0  };
+  MATCH_COLUMNS_T toagg_columns_[] = {TAILOR_COL, RT_DIFF_COL, PRECURSOR_INTENSITY_RANK_M0_COL, DYN_FRAGMENT_PVALUE_COL, COELUTE_MS1_COL };
+  double toagg_coeffs_[] = {1.0, -coeff_rtdiff, -coeff_precursor, coeff_fragment, coeff_elution  };
 
   for (int idx = 0; idx < sizeof(toagg_columns_)/sizeof(toagg_columns_[0]); idx++) {
 	  MATCH_COLUMNS_T curr_column_id = toagg_columns_[idx];
@@ -134,6 +58,81 @@ void DIAmeterPSMFilter::parseHeader() {
   scan_idx_ = fileReader_->findColumn(get_column_header(SCAN_COL));
   charge_idx_ = fileReader_->findColumn(get_column_header(CHARGE_COL));
   xcorr_idx_ = fileReader_->findColumn(get_column_header(XCORR_SCORE_COL));
-  carp(CARP_DEBUG, "ensemble_idx:%d \t scan_idx:%d \t charge_idx:%d \t xcorr_idx:%d", agg_idx_, scan_idx_, charge_idx_, xcorr_idx_ );
+  carp(CARP_DETAILED_DEBUG, "ensemble_idx:%d \t scan_idx:%d \t charge_idx:%d \t xcorr_idx:%d", agg_idx_, scan_idx_, charge_idx_, xcorr_idx_ );
 
 }
+
+void DIAmeterPSMFilter::calcBaseline() {
+	fileReader_->reset();
+	scan_charge_scores_map.clear();
+
+	while (fileReader_->hasNext()) {
+		int scan = fileReader_->getInteger(scan_idx_);
+		int charge = fileReader_->getInteger(charge_idx_);
+		int key = getKey(scan, charge);
+
+		std::vector<std::string> data = fileReader_->getCurrentRowData();
+
+		double xcorr = fileReader_->getDouble(xcorr_idx_);
+		double ensemble = 0.0;
+		for (int idx = 0; idx < toagg_column_indices_.size(); idx++) {
+			int curr_column_idx = toagg_column_indices_.at(idx);
+			double column_val = fileReader_->getDouble(curr_column_idx);
+			double curr_column_coeff = toagg_column_coeffs_.at(idx);
+			ensemble += column_val * curr_column_coeff;
+		}
+
+		map<int, boost::tuple<double, double>>::iterator baselineIter = scan_charge_scores_map.find(key);
+		if (baselineIter == scan_charge_scores_map.end()) { scan_charge_scores_map[key] = boost::make_tuple(xcorr, ensemble); }
+		else {
+			double xcorr_old = (baselineIter->second).get<0>();
+			if (xcorr_old < xcorr) {
+				scan_charge_scores_map[key] = boost::make_tuple(xcorr, ensemble);
+				// carp(CARP_DETAILED_DEBUG, "Update Xcorr! scan:%d \t charge:%d \t xcorr_old:%f \t xcorr_new:%f", scan, charge, xcorr_old, xcorr );
+			}
+		}
+
+		fileReader_->next();
+	}
+}
+
+void DIAmeterPSMFilter::loadAndFilter(const char* output_file_name, bool filter) {
+	calcBaseline();
+	fileReader_->reset();
+
+	ofstream* output_file = create_stream_in_path(output_file_name, NULL, Params::GetBool("overwrite"));
+	*output_file << StringUtils::Join(fileReader_->getColumnNames(), '\t').c_str() << endl;
+
+	while (fileReader_->hasNext()) {
+		int scan = fileReader_->getInteger(scan_idx_);
+		int charge = fileReader_->getInteger(charge_idx_);
+		int key = getKey(scan, charge);
+
+		std::vector<std::string> data = fileReader_->getCurrentRowData();
+
+		double xcorr = fileReader_->getDouble(xcorr_idx_);
+		double ensemble = 0.0;
+		for (int idx = 0; idx < toagg_column_indices_.size(); idx++) {
+			int curr_column_idx = toagg_column_indices_.at(idx);
+			double column_val = fileReader_->getDouble(curr_column_idx);
+			double curr_column_coeff = toagg_column_coeffs_.at(idx);
+			ensemble += column_val * curr_column_coeff;
+		}
+		data[agg_idx_] = StringUtils::ToString<double>(ensemble, 6);
+
+		map<int, boost::tuple<double, double>>::iterator baselineIter = scan_charge_scores_map.find(key);
+		if (baselineIter == scan_charge_scores_map.end()) { carp(CARP_FATAL, "The key must exist in scan_charge_scores_map! %d", key); }
+
+		double xcorr_baseline = (baselineIter->second).get<0>();
+		double ensemble_baseline = (baselineIter->second).get<1>() - 0.000001;
+		if (filter && (ensemble >= ensemble_baseline)) {
+			*output_file << StringUtils::Join(data, '\t').c_str() << endl;
+			carp(CARP_DEBUG, "scan:%d \t charge:%d \t xcorr_baseline.vs.curr:(%f, %f) \t ensemble_baseline.vs.curr:(%f, %f)", scan, charge, xcorr_baseline, xcorr, ensemble_baseline, ensemble);
+		}
+
+		fileReader_->next();
+	}
+
+	if (output_file) { output_file->close(); delete output_file; }
+}
+
