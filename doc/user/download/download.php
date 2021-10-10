795f8253d1b1eee9f3d9890d3f1246b114c94e25
Charles E. Grant <cegrant@uw.edu>
2021-10-09 19:46:46 -0700

    Updated version number

--- doc/user/download/download.php
+++ doc/user/download/download.php
@@ -45,12 +45,12 @@
   # Set names of files available for download
    if (isset($_POST['release_button'])) {
      $FileNameBaseArray = array(
-       "Source" => "crux-4.0.Source.tar.gz",
-       "Linux32" => "crux-4.0.Linux.i686.zip",
-       "Linux64" => "crux-4.0.Linux.x86_64.zip",
-       "OSX" => "crux-4.0.Darwin.x86_64.zip",
-       "Windows64" => "crux-4.0.Windows.AMD64.zip",
-       "Windows32" => "crux-4.0.Windows.i386.zip",
+       "Source" => "crux-4.1.Source.tar.gz",
+       "Linux32" => "crux-4.1.Linux.i686.zip",
+       "Linux64" => "crux-4.1.Linux.x86_64.zip",
+       "OSX" => "crux-4.1.Darwin.x86_64.zip",
+       "Windows64" => "crux-4.1.Windows.AMD64.zip",
+       "Windows32" => "crux-4.1.Windows.i386.zip",
      );
      $directory = "crux-4.0/";
    }
@@ -59,12 +59,12 @@
       # Read commit number from file.
       $version = chop(file_get_contents("daily/latest-build.txt"));
       $FileNameBaseArray = array(
-        "Source" => "crux-4.0.$version.Source.tar.gz",
-        "Linux32" => "crux-4.0.$version.Linux.i686.zip",
-        "Linux64" => "crux-4.0.$version.Linux.x86_64.zip",
+        "Source" => "crux-4.1.$version.Source.tar.gz",
+        "Linux32" => "crux-4.1.$version.Linux.i686.zip",
+        "Linux64" => "crux-4.1.$version.Linux.x86_64.zip",
         "OSX" => "crux-4.0.$version.Darwin.x86_64.zip",
-        "Windows64" => "crux-4.0.$version.Windows.AMD64.zip",
-        "Windows32" => "crux-4.0.$version.Windows.i386.zip",
+        "Windows64" => "crux-4.1.$version.Windows.AMD64.zip",
+        "Windows32" => "crux-4.1.$version.Windows.i386.zip",
       );
       $directory = "daily/";
     }
