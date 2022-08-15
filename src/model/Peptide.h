79430245f8c2174f64dec1d5962f5041ac4db4f0
Attila <kfattila@yandex.com>
2022-08-15 18:29:33 +0300

    removed debugging code

--- src/model/Peptide.h
+++ src/model/Peptide.h
@@ -420,10 +420,6 @@ class Peptide {
     Peptide* peptide_one,
     Peptide* peptide_two
     );
-  static bool lessThanStr(
-    Peptide* peptide_one,
-    Peptide* peptide_two
-    );
 
   /**
    * \brief Builds a comma delimited string listing the 
