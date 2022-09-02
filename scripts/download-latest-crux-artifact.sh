32ec0a175f5e798bde487cea6f856f341330c7b8
CharlesEGrant <cegrant@uw.edu>
2022-09-01 21:18:15 -0700

    Fixes to download script

--- scripts/download-latest-crux-artifact.sh
+++ scripts/download-latest-crux-artifact.sh
@@ -13,11 +13,19 @@ echo "old version is " $old_version
 echo "new version is " $new_version
 if [ "$new_version" != "$old_version" ];
 then
-  echo versions were different
-fi
 # Get the id of the latest run.
 id_latest_run=$(gh run list -R github.com/CharlesEGrant/crux-toolkit -b master --workflow main.yml | \
   grep -oh "success.*"| head -n 1|awk '{print $7}')
 echo $id_latest_run
 # Download the artifacts for the latest run
 gh run download $id_latest_run --pattern "crux*.*" -R github.com/CharlesEGrant/crux-toolkit 
+# Copy artifacts to web site
+cp crux-4.1.$new_version.Source/crux-4.1.Source.tar.gz /noble/www/htdocs/crux-downloads/daily/crux-4.1.$new_version.Source.tar.gz
+cp crux-4.1.$new_version.windows/crux-4.1.Windows.i386.zip /noble/www/htdocs/crux-downloads/daily/crux-4.1.$new_version.Windows.AMD64.zip 
+cp crux-4.1.$new_version.macos/crux-4.1.Darwin.x86_64.zip /noble/www/htdocs/crux-downloads/daily/crux-4.1.$new_version.Darwin.x86_64.zip
+cp crux-4.1.$new_version.centos7/crux-4.1.Linux.x86_64.zip /noble/www/htdocs/crux-downloads/daily/crux-4.1.$new_version.Linux.x86_64.zip
+cp latest-build.txt /noble/www/htdocs/crux-downloads/daily/latest-build.txt
+rm -rf $temp_dir
+else
+  echo "No new build available."
+fi
