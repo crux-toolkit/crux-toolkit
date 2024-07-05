d14f2a31de48d89b8a8ce1a636d87367dc052d8a
codeworse <vladvova77@gmail.com>
2024-07-05 17:22:33 +0300

    Update parsing peaks in mstoolkit

--- src/model/Spectrum.cpp
+++ src/model/Spectrum.cpp
@@ -951,6 +951,24 @@ void Spectrum::sortPeaks(PEAK_SORT_TYPE_T type)
 }
 
 /**
+ * Put the highest peak to the end
+ */
+void Spectrum::putHighestPeak() {
+  if (sorted_by_mz_ || peaks_.size() == 1) {
+    return;
+  }
+  size_t max_mz_peak_index = 0;
+  for (size_t i = 1; i < peaks_.size(); ++i) {
+    if (Peak::compareByMZ(*peaks_[max_mz_peak_index], *peaks_[i])) {
+      max_mz_peak_index = i;
+    }
+  }
+  Peak* temp = peaks_[max_mz_peak_index];
+  peaks_[max_mz_peak_index] = peaks_.back();
+  peaks_.back() = temp;
+}
+
+/**
  * Populate peaks with rank information.
  */
 void Spectrum::rankPeaks()
