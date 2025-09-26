e1510e02b741f8cbb819c09a6b26137b21eb875e
codeworse <vladvova77@gmail.com>
2025-09-26 12:13:38 +0300

    update ions storage

--- src/app/tide/IonInvertedIndex.h
+++ src/app/tide/IonInvertedIndex.h
@@ -12,17 +12,18 @@ class Peptide;
 class IonInvertedIndex {
   public:
     // maps a peak index ix to a list of peptides which contains that peak ix as a theoretical peak.
-    static constexpr size_t kIonStorageSize = 900000;
-    using IonsStorage = std::array<std::deque<std::pair<double, Peptide*>>, kIonStorageSize + 1>;
+    using IonsStorage = std::vector<std::deque<std::pair<double, Peptide*>>>;
 
     IonInvertedIndex(){
       // IonStorage should be a vector with a fixed length, not a map. Each vector component is a dequeue. 
       // Each vector index is associated to a peak_mz;
-       capacity_ = 900000;
-      //   SCORE_FUNCTION_T curScoreFunction = string_to_score_function_type(Params::GetString("score-function"));  
-      // if (curScoreFunction == HYPERSCORE) {
-      //    capacity_ =  MassConstants::mass2bin(MAX_THEORETICAL_PEAK_MZ, 1); 
-      // }
+       capacity_ = 0;
+      SCORE_FUNCTION_T curScoreFunction = string_to_score_function_type(Params::GetString("score-function"));  
+      if (curScoreFunction == HYPERSCORE) {
+         capacity_ =  MassConstants::mass2bin(MAX_THEORETICAL_PEAK_MZ, 1); 
+      }
+      ions_b_.resize(capacity_);
+      ions_y_.resize(capacity_);
       // Create a vector with a length of capacity.
     }
 
